0ec597c15f7b07318d4da439045db05189f013be
ibrahim.yock@gmail.com <ibrahim.yock@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-06-26 09:39:29 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@31 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/model/Scenario.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/model/Scenario.java
@@ -1,12 +1,13 @@
 package learningresourcefinder.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import javax.persistence.ManyToOne;
 
 @Entity
 public class Scenario extends BaseEntity {
-	
+	@Column(columnDefinition = "VARCHAR(50)")
 	private String name;
 	
 	@ManyToOne
