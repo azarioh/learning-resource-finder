05e6e4419821c85d686d970ad1b9249703d651cc
ibrahim.yock@gmail.com <ibrahim.yock@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-06-27 14:04:04 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@47 59821364-4b7c-4859-2266-30ef78c0912a

---
+++ trunk/LearningResourceFinder/src/learningresourcefinder/batch/ResourceBatch.java
@@ -0,0 +1,39 @@
+package learningresourcefinder.batch;
+
+import learningresourcefinder.model.Resource;
+import learningresourcefinder.repository.ResourceRepository;
+import learningresourcefinder.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResourceBatch implements Runnable {
+
+	Resource r = new Resource();
+
+	@Autowired
+	ResourceRepository resourceRepository;
+
+	@Autowired
+	UserRepository userRepository;
+
+	public static void main(String[] args) {
+		BatchUtil.startSpringBatch(ResourceBatch.class);
+	}
+
+	public void insertResource() {
+		r.setDescription("cours de français pour débutant");
+		r.setTitle("Français");
+		r.setUser(userRepository.getUserByUserName("tato"));
+		resourceRepository.persist(r);
+
+	}
+
+	@Override
+	public void run() {
+		insertResource();
+		System.out.println("Done");
+
+	}
+}
