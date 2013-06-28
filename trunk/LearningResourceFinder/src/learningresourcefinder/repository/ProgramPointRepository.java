cf43c7f6539c2e59b619ef08c4444037c0bad4a4
ibrahim.yock@gmail.com <ibrahim.yock@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-06-28 14:06:42 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@74 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/repository/ProgramPointRepository.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/repository/ProgramPointRepository.java
@@ -1,10 +1,26 @@
 package learningresourcefinder.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import learningresourcefinder.model.ProgramPoint;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
+@SuppressWarnings("unchecked")
 public class ProgramPointRepository extends BaseRepository<ProgramPoint> {
 
+	public List<ProgramPoint> findResourceByProgramPointAndSubs(ProgramPoint startProgramPoint) {
+		// 1. We recursively collect all the sub-program points
+		List<ProgramPoint> programPoints = startProgramPoint.getChildrenAndSubChildren();
+		
+		// 2. We query
+		List<ProgramPoint> result = em
+				.createQuery("SELECT r FROM Resource r  WHERE r.programPoint in (:programPoints)")
+				.setParameter("programPoints", programPoints)
+				.getResultList();
+
+		return result;
+	}
 }
