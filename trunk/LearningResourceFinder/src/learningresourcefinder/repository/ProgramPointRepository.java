0d082ac4930e304f30674a21a164ec2637565faf
ibrahim.yock@gmail.com <ibrahim.yock@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-06-28 13:24:39 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@66 59821364-4b7c-4859-2266-30ef78c0912a

---
+++ trunk/LearningResourceFinder/src/learningresourcefinder/repository/ProgramPointRepository.java
@@ -0,0 +1,10 @@
+package learningresourcefinder.repository;
+
+import learningresourcefinder.model.ProgramPoint;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProgramPointRepository extends BaseRepository<ProgramPoint> {
+
+}
