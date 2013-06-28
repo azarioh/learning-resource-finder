643175673973a362c847f96ac402505b0a339949
ibrahim.yock@gmail.com <ibrahim.yock@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-06-28 13:26:37 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@68 59821364-4b7c-4859-2266-30ef78c0912a

---
+++ trunk/LearningResourceFinder/src/learningresourcefinder/batch/ProgramPointBatch.java
@@ -0,0 +1,40 @@
+package learningresourcefinder.batch;
+
+import learningresourcefinder.model.ProgramPoint;
+import learningresourcefinder.repository.ProgramPointRepository;
+import learningresourcefinder.repository.ResourceRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProgramPointBatch implements Runnable {
+	
+	ProgramPoint p = new ProgramPoint();
+	
+	@Autowired
+	ProgramPointRepository programPointRepository;
+	
+	@Autowired
+	ResourceRepository resourceRepositiry;
+
+	public static void main(String[] args) {
+		BatchUtil.startSpringBatch(ProgramPointBatch.class);
+	}
+	
+	public void insertProgram () {
+		
+		p.setDescription("première primaire");
+		p.setDuration(180);
+		p.setLevel(2);
+		p.setName("Première primaire ( 1P )");
+		p.addResource(resourceRepositiry.getResourceByTitle("Français"));
+		programPointRepository.persist(p);
+	}
+	
+	@Override
+	public void run() {
+		insertProgram();
+		
+	}
+}
