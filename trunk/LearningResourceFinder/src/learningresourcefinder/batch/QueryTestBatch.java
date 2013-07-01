437ea53853292c7e747a04e6dccd87e4c6c95634
thomasdelizee@gmail.com <thomasdelizee@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-07-01 13:53:38 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@105 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/batch/QueryTestBatch.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/batch/QueryTestBatch.java
@@ -5,8 +5,11 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import learningresourcefinder.model.Problem;
 import learningresourcefinder.model.ProgramPoint;
 import learningresourcefinder.model.Resource;
+import learningresourcefinder.model.Task;
+import learningresourcefinder.model.User;
 import learningresourcefinder.repository.ProgramPointRepository;
 import learningresourcefinder.repository.ResourceRepository;
 
@@ -28,6 +31,20 @@ public class QueryTestBatch implements Runnable {
 	
 	@Override
 	public void run() {
+		ProgramPoint pFond = programPointRepository.findByCode("Fon");
+		List<Resource> resList = programPointRepository.findResourceByProgramPointAndSubs(pFond);
+		System.out.println(resList);		
+		
+		//User u = userRepository.getUserByUserName("deli");
+		//List<Task> taskList = taskRepository.findTasksOfAssigner(u);
+		//System.out.println("La liste des tâches : " + taskList.toString());
+		
+		//List<Problem> problemList = problemRepository.findProblemOfAuthor(u);
+		//System.out.println("La liste des problèmes : " + problemList.toString());
+		
+		//List<Resource> resourceList = resourceRepository.findAllResourceByUser(u);
+		//System.out.println("Les ressources d'un auteur : " + resourceList);
+		
 //		ProgramPoint p2 = programPointRepository.findByCode("2p");
 //		Resource mathGob = resourceRepository.getResourceByName("Goblin-Math");
 //		p2.addResource(mathGob);
