68a02671966bf37c5d78eb0e56927145a2f5be18
ahmed.idoumhaidi@gmail.com <ahmed.idoumhaidi@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-07-01 11:05:30 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@99 59821364-4b7c-4859-2266-30ef78c0912a

---
+++ trunk/LearningResourceFinder/src/learningresourcefinder/search/Searchable.java
@@ -0,0 +1,15 @@
+package learningresourcefinder.search;
+
+import java.util.Map;
+
+public interface Searchable {
+    
+    
+    public Map<String ,String>  getCriterias();
+    
+    public String getBoostedCriteriaName();
+    
+    public Long getId();
+    
+    
+}
