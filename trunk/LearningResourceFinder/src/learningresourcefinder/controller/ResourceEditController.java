5df0f70686f18824445b15301f21db37545347fb
ibrahim.yock@gmail.com <ibrahim.yock@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-08-08 10:24:34 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@173 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/ResourceEditController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/ResourceEditController.java
@@ -1,5 +1,75 @@
 package learningresourcefinder.controller;
 
-public class ResourceEditController {
+import javax.validation.Valid;
+
+import learningresourcefinder.model.Resource;
+import learningresourcefinder.repository.ResourceRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.portlet.ModelAndView;
+
+@Controller
+public class ResourceEditController extends BaseController {
+
+	@Autowired ResourceRepository resourceRepository; 
+@RequestMapping("/resourcecreate")
+public ModelAndView resourceCreate() {
+		return prepareModelAndView(new Resource() );
+}
+
+@RequestMapping("/resourceedit")
+public ModelAndView resourceEdit(@RequestParam("id") long id){
+	
+	Resource resource=(Resource)getRequiredEntity(id, Resource.class);
+	return prepareModelAndView(resource);
+		
+			}
+
+
+private ModelAndView prepareModelAndView(Resource resource) {
+	ModelAndView mv=new ModelAndView("resourceedit"); //JSP
+	mv.addObject("id", resource.getId()); // will be null in case of create
+	mv.addObject("resource", resource);
+	return mv;
+}
+
+@RequestMapping("/resourcesubmit")
+
+public ModelAndView resourceEditSubmit(@Valid @ModelAttribute Resource resource, BindingResult bindingResult) {
+	
+	if (bindingResult.hasErrors()){
+		
+		return new ModelAndView("resourceedit","resource",resource);
+	}
+	if(resource.getId()==null){
+		resourceRepository.persist(resource);
+	}
+		else {resourceRepository.merge(resource);
+		
+		
+	}
+	return new ModelAndView("redirect:/resource?id="+resource.getId());
+}
+
+@ModelAttribute
+public Resource findResource(@RequestParam(value="id",required=false)Long id){
+	if(id==null){
+		//create
+		return new Resource(); 
+	}else {
+		
+		return (Resource)getRequiredDetachedEntity(id);
+				
+	}
+}
+
 
 }
+
+
+
