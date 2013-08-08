c72a6057dcba7d54c5d222238f211e8153290a3d
john.rizzo.be@gmail.com <john.rizzo.be@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-08-08 07:53:07 +0000

    Change password refactor
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@167 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/WebContent/WEB-INF/views/userchangepassword.jsp
+++ trunk/LearningResourceFinder/WebContent/WEB-INF/views/userchangepassword.jsp
@@ -3,29 +3,28 @@
 <%@ page import="learningresourcefinder.security.Privilege"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib uri="http://www.springframework.org/tags/form"  prefix="form"%>
-<%@ taglib tagdir="/WEB-INF/tags/lrftag/" prefix="lrftag" %>
+<%@ taglib tagdir="/WEB-INF/tags/lrftag/" prefix="lrf" %>
 
 
 <html>
 <body>
 <!-- <h1>Formulaire de changement de password</h1> -->
 
-<lrftag:breadcrumb>
-	<lrftag:breadcrumbelement label="${user.firstName} ${user.lastName}" link="/user/${user.userName}" />
-	<lrftag:breadcrumbelement label="changement de mot de passe" />
-</lrftag:breadcrumb>
-<lrftag:pageheadertitle title="changement de mot de passe"/>
-	<form:form modelAttribute="user" action="/user/changepasswordsubmit">
-	<table>
+<lrf:breadcrumb>
+	<lrf:breadcrumbelement label="${user.firstName} ${user.lastName}" link="/user/${user.userName}" />
+	<lrf:breadcrumbelement label="changement de mot de passe" />
+</lrf:breadcrumb>
+<lrf:pageheadertitle title="changement de mot de passe"/>
+
+
+	<lrf:form modelAttribute="passwordData" action="/user/changepasswordsubmit">
 			<c:if test="${verifyOldPassword}">
-		 		 <tr><td>Password actuel: </td><td><input type="password" name="oldPassword" /></td>
-		   		 <td>${errorNoOld}</td></tr>
+		 		 <lrf:input label="Mot de passe actuel" path="oldPassword" type="password" />
 		    </c:if>
-		<tr><td>Nouveau mot de passe: </td><td><input type="password" name="newPassword" /></td><td>${errorEmpty}${errorDiff}</td></tr>
-		<tr><td>Confirmer le nouveau password: </td><td><input type="password" name="confirmPassword" /></td><td>${errorEmpty}${errorDiff}</td></tr>
-		<form:hidden path="id" />
+		    <lrf:input label="Nouveau mot de passe" path="newPassword" type="password" />
+		    <lrf:input label="Confirmer le nouveau  mot de passe" path="confirmPassword" type="password" />
+		<input type="hidden" name="id" value="${user.id}" />
 		<tr><td><input type="submit" value="changer"/></td><td><a href="/user/${user.userName}">Annuler</a></td></tr>
-	</table>
-	</form:form>
+	</lrf:form>
 </body>
 </html>
--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/UserChangePasswordController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/UserChangePasswordController.java
@@ -1,9 +1,13 @@
 package learningresourcefinder.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
+
+import learningresourcefinder.model.User;
+import learningresourcefinder.repository.UserRepository;
+import learningresourcefinder.security.Privilege;
+import learningresourcefinder.security.SecurityContext;
+import learningresourcefinder.util.NotificationUtil;
+import learningresourcefinder.util.SecurityUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,66 +17,88 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import learningresourcefinder.model.User;
-import learningresourcefinder.repository.UserRepository;
-import learningresourcefinder.security.Privilege;
-import learningresourcefinder.security.SecurityContext;
-import learningresourcefinder.util.NotificationUtil;
-import learningresourcefinder.util.SecurityUtils;
-
 @Controller
 @RequestMapping ("/user")
-public class UserChangePasswordController {
+public class UserChangePasswordController extends BaseController<User> {
 
+	// Value object matching the form fields (ModelAttribute)
+	static class PasswordData {
+		String oldPassword;
+		String newPassword;
+		String confirmPassword;
+		public String getOldPassword() {
+			return oldPassword;
+		}
+		public void setOldPassword(String oldPassword) {
+			this.oldPassword = oldPassword;
+		}
+		public String getNewPassword() {
+			return newPassword;
+		}
+		public void setNewPassword(String newPassword) {
+			this.newPassword = newPassword;
+		}
+		public String getConfirmPassword() {
+			return confirmPassword;
+		}
+		public void setConfirmPassword(String confirmPassword) {
+			this.confirmPassword = confirmPassword;
+		}
+	}
+	
+	
 	@Autowired UserRepository userRepository;
 
 	@RequestMapping("/changepassword")
-	public ModelAndView userChangePassword(@ModelAttribute User user) {
-
-		ModelAndView mv= new ModelAndView("userchangepassword");
-		mv.addObject("user",user);
-		mv.addObject("verifyOldPassword", needToVerifyOldPassword(user));
-		return mv;
-	}
-
-	@RequestMapping("/changepasswordsubmit")
-	public ModelAndView userChangePasswordSubmit(@Valid @ModelAttribute User user, BindingResult result,
-			@RequestParam(value= "oldPassword",  required=false) String oldPassword,
-			@RequestParam("newPassword") String newPassword,
-			@RequestParam("confirmPassword") String confirmPassword) {
-
+	public ModelAndView userChangePassword(@RequestParam("id") long id) {
+		User user = getRequiredEntity(id);
 		SecurityContext.assertCurrentUserMayEditThisUser(user);
 
-		if (result.hasErrors()) {
+		ModelAndView formMv = new ModelAndView("userchangepassword");
+		formMv.addObject("user",user);
+		formMv.addObject("verifyOldPassword", needToVerifyOldPassword(user));
+		formMv.addObject("passwordData", new PasswordData());
+		return formMv;
+	}
+
+	
+	@RequestMapping("/changepasswordsubmit")
+	public ModelAndView userChangePasswordSubmit(@Valid @ModelAttribute PasswordData passwordData, BindingResult bindingResult, @RequestParam("id") long id) {
+		User user = getRequiredEntity(id);
+		SecurityContext.assertCurrentUserMayEditThisUser(user);
+
+		if (bindingResult.hasErrors()) {
 			return new ModelAndView("redirect:user","id",user.getId());
-		}
-		else {
-			Map<String, String> errorMsgMap = new HashMap<String, String>();
-
-
+			
+		} else {
 
 			if(needToVerifyOldPassword(user)){
-				if (!(SecurityUtils.md5Encode(oldPassword)).equals(user.getPassword())) {
-					errorMsgMap.put("errorNoOld", "Vous n'avez pas bien encodé votre ancien mot de passe");
+				if (!(SecurityUtils.md5Encode(passwordData.oldPassword)).equals(user.getPassword())) {
+				    bindingResult.rejectValue("oldPassword", "", "Le mot de passe que vous avez introduit ne correspond pas au mot de passe que nous connaissons.");
 				}
 			}
 
-			if (newPassword.equals("") || confirmPassword.equals("")) {
-				errorMsgMap.put("errorEmpty", "Veuillez encoder un nouveau mot de passe et/ou la confirmation de ce mot de passe");
-			} else if (!confirmPassword.equals(newPassword)) {
-				errorMsgMap.put("errorDiff", "La confirmation du mot de passe et le mot de passe ne correspondent pas");
+			if (passwordData.newPassword == null || passwordData.newPassword.equals("")) {
+				bindingResult.rejectValue("newPassword", "", "Veuillez introduire un nouveau mot de passe");
+			}
+			if ( passwordData.confirmPassword == null || passwordData.confirmPassword.equals("")) {
+				bindingResult.rejectValue("confirmPassword", "", "Veuillez introduire la confirmation du nouveau mot de passe");
+			}
+			if (passwordData.confirmPassword != null && passwordData.newPassword != null && !passwordData.confirmPassword.equals(passwordData.newPassword)) {
+				String msg = "Le nouveau mot de passe et sa confirmation ne correspondent pas.";
+				bindingResult.rejectValue("newPassword", "", msg);
+				bindingResult.rejectValue("confirmPassword", "", msg);
 			}
 
 
-			if (!errorMsgMap.isEmpty()) {
-				ModelAndView errorDspMv = new ModelAndView("userchangepassword");
-				for (String msgKey : errorMsgMap.keySet()) {
-					errorDspMv.addObject(msgKey, errorMsgMap.get(msgKey));	
-				}
-				errorDspMv.addObject("user", user);
-				return errorDspMv;
+			if (bindingResult.hasErrors()) {
+				ModelAndView formMv = new ModelAndView("userchangepassword");
+				formMv.addObject("user", user);
+				formMv.addObject("verifyOldPassword", needToVerifyOldPassword(user));
+				formMv.addObject("passwordData", passwordData);
+				return formMv;
 			} else {
-				user.setPassword(SecurityUtils.md5Encode(confirmPassword));
+				user.setPassword(SecurityUtils.md5Encode(passwordData.confirmPassword));
 				user.setPasswordKnownByTheUser(true);
 				userRepository.merge(user);
 				NotificationUtil.addNotificationMessage("Votre nouveau mot de passe est bien enregistré");
@@ -94,10 +120,10 @@ public class UserChangePasswordController {
 	}
 	
 
-	@ModelAttribute
-	public User findUser(@RequestParam("id") Long id){
-		return  userRepository.find(id);
-	}
+//	@ModelAttribute
+//	public User findUser(@RequestParam("id") Long id){
+//		return  userRepository.find(id);
+//	}
 
 
 }
