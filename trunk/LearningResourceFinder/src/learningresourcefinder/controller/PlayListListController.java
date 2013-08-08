9cbd1cb329bd4d706f5a819616672c9e01718e30
emilekane@gmail.com <emilekane@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-08-08 14:41:13 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@184 59821364-4b7c-4859-2266-30ef78c0912a

---
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/PlayListListController.java
@@ -0,0 +1,38 @@
+package learningresourcefinder.controller;
+
+import javax.annotation.PostConstruct;
+
+import learningresourcefinder.exception.InvalidUrlException;
+import learningresourcefinder.model.PlayList;
+import learningresourcefinder.model.User;
+import learningresourcefinder.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/playlist")
+public class PlayListListController  extends BaseController<PlayList> {
+
+	@Autowired UserRepository userRepository;
+	
+	@PostConstruct
+	public void test() {
+		System.out.println(userRepository + "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx");
+	}
+	
+	@RequestMapping("/user/{userName}")
+	public ModelAndView displayList(@PathVariable("userName") String userName) {
+		User user = userRepository.getUserByUserName(userName);
+        if (user == null) {
+        	throw new InvalidUrlException("L'utilisateur ayant le pseudonyme (userName) '"+userName+"' est introuvable.");
+        }
+        
+		return new ModelAndView("playlistlist", "playlistlist", user.getPlaylist());
+	}
+
+
+}
