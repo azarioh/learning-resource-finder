5a23c1892425553a59fee7b06415b36515172d39
filippo.giannnini@gmail.com <filippo.giannnini@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-08-09 13:41:33 +0000

    New SQL for urlresource
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@204 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/SQL/v0-0.1.sql
+++ trunk/LearningResourceFinder/SQL/v0-0.1.sql
@@ -1,5 +1,5 @@
 
--- Thomas D 2013-08-09    
+-- Thomas D 2013-08-09  14h00  
 
 create table url_resource (
         id int8 not null,
@@ -25,6 +25,36 @@ create table url_resource (
 
     alter table url_resource 
         add constraint fkd214dcde750f4d4 
+        foreign key (updatedby_id) 
+        references users;
+        
+        
+-- Thomas D 2013-08-09 15h30
+
+        create table urlresource (
+        id int8 not null,
+        createdon timestamp,
+        updatedon timestamp,
+        name varchar(50),
+        url varchar(255),
+        createdby_id int8,
+        updatedby_id int8,
+        resource_id int8,
+        primary key (id)
+    );
+
+    alter table urlresource 
+        add constraint fk4dc4aa7d9ae62ac7 
+        foreign key (createdby_id) 
+        references users;
+
+    alter table urlresource 
+        add constraint fk4dc4aa7dbf94315b 
+        foreign key (resource_id) 
+        references resource;
+
+    alter table urlresource 
+        add constraint fk4dc4aa7d750f4d4 
         foreign key (updatedby_id) 
         references users;
         
