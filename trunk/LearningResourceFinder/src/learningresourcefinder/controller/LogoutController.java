de171fd99e387ae2e9f1208cafceb21c13043214
lionel.gosseries@gmail.com <lionel.gosseries@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-08-12 13:14:16 +0000

    add logout controller
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@230 59821364-4b7c-4859-2266-30ef78c0912a

---
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/LogoutController.java
@@ -0,0 +1,44 @@
+package learningresourcefinder.controller;
+
+
+
+
+import learningresourcefinder.model.User;
+import learningresourcefinder.service.LoginService;
+import learningresourcefinder.util.NotificationUtil;
+import learningresourcefinder.web.UrlUtil;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+
+
+
+
+
+@Controller
+public class LogoutController extends BaseController<User>{
+    
+    @Autowired LoginService loginService;
+
+    @RequestMapping("/logout")
+    public String logout(WebRequest request) {
+  
+         loginService.logout();
+         NotificationUtil.addNotificationMessage("Vous ête à present deconnecté de "+UrlUtil.getWebSiteName());
+         
+        return "redirect:/";
+    }
+    @RequestMapping("/ajax/logout")
+    public ResponseEntity<String> logoutAjax() {
+   
+         loginService.logout();
+         
+         
+        return new ResponseEntity<String>("loged out", HttpStatus.OK);
+    }
+   
+}
