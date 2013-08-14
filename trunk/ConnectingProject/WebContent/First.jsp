0639921683a7c286f375ab393e3e34ca7844170c
ahmed.idoumhaidi@gmail.com <ahmed.idoumhaidi@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-08-14 07:53:41 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@261 59821364-4b7c-4859-2266-30ef78c0912a

---
+++ trunk/ConnectingProject/WebContent/First.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+<h1> Facebook </h1>
+
+<a href="https://www.facebook.com/dialog/oauth?client_id=350985091585275&redirect_uri=http://localhost:8080/ConnectingProject/Profile">Connexion</a>
+
+</body>
+</html>
---
+++ trunk/ConnectingProject/src/be/demos/fb/SecondServlet.java
@@ -0,0 +1,64 @@
+package be.demos.fb;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.DefaultWebRequestor;
+import com.restfb.FacebookClient;
+import com.restfb.WebRequestor;
+import com.restfb.types.User;
+
+public class SecondServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private final String APP_ID = "350985091585275";
+	private final String SECRET_KEY = "3de40a5fe635e975a7d0d8e10719daa4";
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		FacebookClient.AccessToken token = getFacebookUserToken(req.getParameter("code"), "http://localhost:8080/ConnectingProject/Profile");
+		
+		String accessToken = token.getAccessToken();
+		
+		FacebookClient fclient = new DefaultFacebookClient(accessToken);
+	 
+		User u = fclient.fetchObject(APP_ID,User.class);
+		
+		System.out.println(token);
+		
+		System.out.println(accessToken);
+		
+		req.setAttribute("email", u.getBirthday());
+	 
+		this.getServletContext().getRequestDispatcher("/Second.jsp").forward(req, resp);
+		
+	}
+	
+	private FacebookClient.AccessToken getFacebookUserToken(String code, String redirectUrl) throws IOException {
+	    String appId = APP_ID;
+	    String secretKey = SECRET_KEY;
+
+	    WebRequestor wr = new DefaultWebRequestor();
+	    WebRequestor.Response accessTokenResponse = wr.executeGet(
+	            "https://graph.facebook.com/oauth/access_token?client_id=" + appId + "&redirect_uri=" + redirectUrl
+	            + "&client_secret=" + secretKey + "&code=" + code);
+
+	    return  DefaultFacebookClient.AccessToken.fromQueryString(accessTokenResponse.getBody()); 
+	}
+	
+	
+
+
+}
