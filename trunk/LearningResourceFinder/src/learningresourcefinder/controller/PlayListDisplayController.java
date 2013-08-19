0fe11a2d647df68c982cb2bea91368bf71a3555e
emilekane@gmail.com <emilekane@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-08-19 14:54:50 +0000

    add redirect playlist
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@287 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/PlayListDisplayController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/PlayListDisplayController.java
@@ -3,6 +3,7 @@ package learningresourcefinder.controller;
 import learningresourcefinder.model.PlayList;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,8 +11,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PlayListDisplayController extends BaseController<PlayList> {
 
-	@RequestMapping("/playlist")
-	public ModelAndView prepareModelAndView(@RequestParam("id") long id) {
+	@RequestMapping("/playlist/{id}/{slug}")
+	public ModelAndView prepareModelAndView(@PathVariable long id) {
 		PlayList playlist = getRequiredEntity(id);
 		return new ModelAndView("playlistdisplay", "playlist", playlist);
 	}
