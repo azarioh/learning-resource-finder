b90ab5fe95ce97e9b28505e2a25247ea629f31b1
Thomas.Delizee@gmail.com <Thomas.Delizee@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-08-23 13:11:44 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@366 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/WebContent/js/int/resourceImageGallery.js
+++ trunk/LearningResourceFinder/WebContent/js/int/resourceImageGallery.js
@@ -1,9 +1,9 @@
 $(document).ready( function(){ // quand la page a fini de se charger
 	$("#list-photos").sortable({ // initialisation de Sortable sur #list-photos
-		
 		placeholder: 'highlight', // classe à ajouter à l'élément fantome
     	update: function() {  // callback quand l'ordre de la liste est changé
     		var order = $('#list-photos').sortable('serialize'); // récupération des données à envoyer
+    		alert(order);
     		$.post('/resource/change',order); // appel ajax au fichier ajax.php avec l'ordre des photos
     	}
 	});
