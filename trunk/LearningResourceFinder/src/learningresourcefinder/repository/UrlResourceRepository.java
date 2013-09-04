6f73ba1d4686b80942ade1550b9eeacbd8ff0be0
emilekane@gmail.com <emilekane@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-04 14:32:01 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@426 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/repository/UrlResourceRepository.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/repository/UrlResourceRepository.java
@@ -1,11 +1,16 @@
 package learningresourcefinder.repository;
 
-import learningresourcefinder.model.UrlResource;
 
+import learningresourcefinder.model.UrlResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @SuppressWarnings("unchecked")
 public class UrlResourceRepository extends BaseRepository<UrlResource> {
-
+	
+	public UrlResource getResourceByUrl(String url) {
+		return getSingleOrNullResult(em.createQuery("SELECT u FROM UrlResource r WHERE u.url =:url")
+                .setParameter("url", url));
+	}
+	
 }
