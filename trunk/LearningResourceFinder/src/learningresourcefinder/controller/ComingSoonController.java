01da50e037f7c970e782559813b48500b85b5fcb
isyapan.cz@gmail.com <isyapan.cz@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-09 09:12:16 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@456 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/ComingSoonController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/ComingSoonController.java
@@ -7,14 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class ComingSoonController extends BaseController<ComingSoonMail> {
 
 	@RequestMapping(value="/ajax/addMailOnTable", method = RequestMethod.POST)
-	public @ResponseBody String addMailOnTable( @RequestParam("mail") String emailSent){
+	public @ResponseBody String addMailOnTable( @RequestParam("mail") String addresemail){
+		
 		ComingSoonMail comingSoonMail = new ComingSoonMail();	
-		comingSoonMail.setEmail(emailSent);
+		comingSoonMail.setEmail(addresemail);
 		em.persist(comingSoonMail);
 		
 		return "OK";  // We must return something (else the browser thinks it's an error case), but the value is not needed by our javascript code on the browser.
