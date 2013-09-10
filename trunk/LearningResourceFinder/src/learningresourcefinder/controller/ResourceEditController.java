e923ce2be7be2f43f7b4eb514836723616b56cc6
ibrahim.yock@gmail.com <ibrahim.yock@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-10 11:46:01 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@526 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/ResourceEditController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/ResourceEditController.java
@@ -1,7 +1,5 @@
 package learningresourcefinder.controller;
 
-import javax.validation.Valid;
-
 import learningresourcefinder.model.Resource;
 import learningresourcefinder.model.UrlResource;
 import learningresourcefinder.repository.ResourceRepository;
@@ -11,94 +9,58 @@ import learningresourcefinder.web.Slugify;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ResourceEditController extends BaseController<Resource> {
 
-	@Autowired ResourceRepository resourceRepository; 
-	@Autowired UrlResourceRepository urlResourceRepository;
-	@RequestMapping("/resourcecreate")
-	public ModelAndView resourceCreate() {
-		return prepareModelAndView(new Resource() );
-	}
+    @Autowired
+    ResourceRepository resourceRepository;
+    @Autowired
+    UrlResourceRepository urlResourceRepository;
 
-//	@RequestMapping("/resourceedit")
-//	public ModelAndView resourceEdit(@RequestParam("id") long id){
-//        Resource resource=(Resource)getRequiredEntity(id, Resource.class);
-//		return prepareModelAndView(resource);
-//
-//	}
-	
-    @RequestMapping("/resourceedit")
-    public ModelAndView resourceEdit(@RequestParam("id") long id){
-        Resource resource=(Resource)getRequiredEntity(id, Resource.class);
-        SecurityContext.assertCurrentUserMayEditThisPlaylist(resource);
-        return prepareModelAndView(resource);
+    @RequestMapping("/ajax/resourceeditsubmit")
+    public @ResponseBody
+    String resourceAddSubmit(@RequestParam("url") String url,
+            @RequestParam("title") String title,
+            @RequestParam("description") String description) {
 
+        SecurityContext.assertUserIsLoggedIn();
+
+        Resource resource = new Resource();
+        resource.setDescription(description);
+        resource.setName(title);
+
+        UrlResource urlResource = new UrlResource();
+        urlResource.setName(title);
+        urlResource.setUrl(url);
+        urlResource.setResource(resource);
+
+        resource.getUrlResources().add(urlResource);
+
+        String slug = Slugify.slugify(resource.getName());
+        resource.setSlug(slug);
+
+        resourceRepository.persist(resource);
+
+        // Url to eventually view the resource
+        return "/resource/" + resource.getShortId() + "/" + resource.getSlug();
     }
 
+    @RequestMapping(value = "ajax/checkUrl", method = RequestMethod.POST)
+    public @ResponseBody
+    boolean urlSubmit(@RequestParam("url") String url) {
+        Boolean checkUrlOk = false;
+        if (urlResourceRepository.getResourceByUrl(url) != null)
+            checkUrlOk = true;
+        else
+            checkUrlOk = false;
 
-	private ModelAndView prepareModelAndView(Resource resource) {
-		ModelAndView mv=new ModelAndView("resourceedit"); //JSP
-		mv.addObject("id", resource.getId()); // will be null in case of create
-		mv.addObject("resource", resource);
-		return mv;
-	}
-
-	@RequestMapping("/resourceeditsubmit")
-	public ModelAndView resourceEditSubmit(@Valid @ModelAttribute Resource resource, BindingResult bindingResult) {
-
-		if (bindingResult.hasErrors()){
-			return new ModelAndView("resourceedit","resource",resource);
-		}
-		
-		String slug = Slugify.slugify(resource.getName());
-		resource.setSlug(slug);
-		
-		if(resource.getId()==null){
-			resourceRepository.persist(resource);
-		} else {
-			resourceRepository.merge(resource);
-		}
-		
-		return new ModelAndView("redirect:/resource/"+resource.getShortId()+"/"+resource.getSlug());
-	}
-
-	@ModelAttribute
-	public Resource findResource(@RequestParam(value="id",required=false)Long id){
-		if(id==null){
-			//create
-			return new Resource(); 
-		}else {
-
-			return (Resource)getRequiredDetachedEntity(id);
-
-		}
-	}
-	public int addImageOnDB(){
-		return 0;
-	}
-	
-
-	@RequestMapping(value="ajax/checkUrl",method=RequestMethod.POST)
-	public @ResponseBody boolean urlSubmit(@RequestParam("url") String url) {
-		 Boolean checkUrlOk = false;
-		 if(urlResourceRepository.getResourceByUrl(url) != null)
-			checkUrlOk =true;
-		else
-	    	checkUrlOk =false;
-		
-		return checkUrlOk;
-	}
+        return checkUrlOk;
+    }
 
 }
-
-
-
