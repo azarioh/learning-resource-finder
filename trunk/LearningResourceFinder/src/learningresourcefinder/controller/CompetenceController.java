4c73607a46e464bf6f50707bebc3e84d62fab4bf
jaumottej@gmail.com <jaumottej@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-10 07:53:28 +0000

    rename compController CompetenceController
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@502 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/CompetenceController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/CompetenceController.java
@@ -2,21 +2,42 @@ package learningresourcefinder.controller;
 
 import learningresourcefinder.model.BaseEntity;
 import learningresourcefinder.model.Competence;
+import learningresourcefinder.security.SecurityContext;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class CompetenceController extends BaseController<Competence> {
+public class CompetenceController extends BaseController<BaseEntity> {
     
    @RequestMapping ("/tree")
    public ModelAndView DisplayCompTree (){
-       
-              
-       ModelAndView mv= new ModelAndView("competencetree");
+       ModelAndView mv= new ModelAndView("competencestree");
     return mv; 
    }
+   
+   @RequestMapping({"/competence/test"}) // out of context test of ajax query to add a competence in pgm tree
+	public ModelAndView prepareModelAndView() {
+
+		ModelAndView mv = new ModelAndView("testAjaxCompetence", "competence", null);
+   	
+		return mv;
+	}
+   
+	@RequestMapping(value="/ajax/competenceAddSubmit", method = RequestMethod.POST)
+	public @ResponseBody String ajaxAddCompetence( @RequestParam("name") String nameCompetence, @RequestParam("code") String codeCompetence){
+		
+		if (SecurityContext.canCurrentUserEditCompetence()){
+			Competence competence = new Competence(codeCompetence,nameCompetence);
+			em.persist(competence);
+		}
+		
+		return "OK : name " + nameCompetence  + " code " + codeCompetence;  // We must return something (else the browser thinks it's an error case), but the value is not needed by our javascript code on the browser.
+	}
    
 
 }
