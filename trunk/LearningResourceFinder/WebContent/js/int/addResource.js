7d8f0b12816cb9f638efbb0c800292528926857d
emilekane@gmail.com <emilekane@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-11 14:52:19 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@557 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/WebContent/js/int/addResource.js
+++ trunk/LearningResourceFinder/WebContent/js/int/addResource.js
@@ -1,5 +1,5 @@
 // Sends an ajax request for the user who wants to upload an image from an URL (text field).
-function ajaxPostAddResource() {
+function ajaxVerifyUrl() {
 	var url  = $('#url').val();
 	if(url != '') {
 		
@@ -8,9 +8,14 @@ function ajaxPostAddResource() {
 		    url : 'ajax/checkUrl',
 		    data: "url="+url,
 		    success : function(data) {
-	    		
-		    	if(data == "") {  // No other resource with the same url found in DB.
-		    		 $("#titleDiv").show();
+	    		if(data == "") {  // No other resource with the same url found in DB.
+	    			if(isValidURL(url)==false){
+	    				alert("url non valide ");
+	    			} else {
+		    			 $("#bottomButtons").show();	
+			    		 $("#formPart2").show();
+		    		     $("#urlCheckButton").hide();
+	    			}
 		    	} else{
 		    		alert("une resource avec une url similaire existe déjà sur le site");
 		        }
@@ -22,16 +27,36 @@ function ajaxPostAddResource() {
 		}); 
 	}
 	else {
-		alert('Veuillez entrer une URL valide !');
+		alert('Veuillez entrer une URL valide, commençant par "http://"');
 	}
 	
 }
 
-
-function showTilte() {
-	$("#titleDiv").show();
+function isValidURL(url) {
+	var urlRegxp = /^(http:\/\/www.|https:\/\/www.|http:\/\/){1}([\w]+)(.[\w]+){1,2}$/;
+	if (urlRegxp.test(url) != true) {
+	    return false;
+	} else {
+		return true;
+	}
 }
 
- 
+
+
+function ajaxResourceAddSubmit() {
+	$.ajax({
+			type : "POST",
+		    url : 'ajax/resourceaddsubmit',
+		    data: $("#addResourceForm").serialize(),
+		    success : function(msg) {
+	    		alert(msg);
+	    		$('#myModal').modal('hide');
+		    },
+		    error : function(data) {
+		    	alert("Suite à un problème du côté serveur, la ressource n'a probablement pas pu être ajoutée. - "+ data);
+		    }
+	}); 
+	
+}
 
 
