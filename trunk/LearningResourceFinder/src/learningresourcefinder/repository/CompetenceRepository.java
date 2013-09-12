28286c73a929201533a2eb410d3f1b4314645750
jaumottej@gmail.com <jaumottej@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-12 12:12:23 +0000

    new methods in repository
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@572 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/repository/CompetenceRepository.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/repository/CompetenceRepository.java
@@ -15,7 +15,7 @@ public class CompetenceRepository extends BaseRepository<Competence> {
 		// 1. We recursively collect all the sub-program points
 		List<Competence> competences = startCompetence.getChildrenAndSubChildren();
 		competences.add(startCompetence);
-		
+
 		// 2. We query
 		List<Resource> result = em
 				.createQuery("SELECT distinct r FROM Resource r join r.competences c WHERE c in (:competences)")
@@ -24,16 +24,51 @@ public class CompetenceRepository extends BaseRepository<Competence> {
 
 		return result;
 	}
+
+
+	public Competence findByCode(String code){
+		return getSingleOrNullResult(
+				em.createQuery("select c from Competence c where lower(c.code) = :code")
+				.setParameter("code",code.toLowerCase())
+				);
+	}
+
+	public List<Competence> findAllWithoutParent(){
+		return    em.createQuery("select c from Competence c where c.parent is null ").getResultList();
+	}
+
+
+	public boolean getIfCompetenceCodeExistOrIsCurrentlyBeingEdited(Long id,String codeCompetence) {
+		
+		List<Competence> result = em
+				.createQuery("select c from Competence c where lower(c.code) = :code")
+				.setParameter("code", codeCompetence.toLowerCase())
+				.getResultList();
+		
+		switch (result.size()){
+			case 0: return true;
+			case 1: if (result.get(0).getId() == id ) {return true;}else{return false;}
+			default : return false;
+		}
+	}
+
+
+	public boolean getIfParentExist(Long parentIdCompetence) {
+		
+		return (em
+				.createQuery("select c from Competence c where c.id = :id")
+				.setParameter("id", parentIdCompetence)
+				.getResultList()).size() > 0;
+	}
+
+
+	public boolean getIfCodeExist(String codeCompetence) {
 	
-	
-    public Competence findByCode(String code){
-        return getSingleOrNullResult(
-        		em.createQuery("select c from Competence c where lower(c.code) = :code")
-        		.setParameter("code",code.toLowerCase())
-        );
-    }
-    public List<Competence> findAllWithoutParent(){
-        return    em.createQuery("select c from Competence c where c.parent is null ").getResultList();
-    }
-    
+		return (em
+				.createQuery("select c from Competence c where lower(c.code) = :code")
+				.setParameter("code", codeCompetence.toLowerCase())
+				.getResultList()).size() != 0;
+	}
+
+
 }
