03a0fb3551358c4b5f6bfe89af72d84bf1bafced
isyapan.cz@gmail.com <isyapan.cz@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-12 12:18:39 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@575 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/SearchSummaryController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/SearchSummaryController.java
@@ -12,6 +12,7 @@ import learningresourcefinder.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -21,21 +22,24 @@ public class SearchSummaryController extends BaseController<Resource> {
 	@Autowired IndexManagerService indexService;
 	@Autowired ResourceRepository resourceRepository;
 
-	@RequestMapping("/searchresource")
-	public ModelAndView getAllResource(){
-
-		List<SearchResult> searchListe = searchService.search("mathématique");
-		List<Resource> resourceList = new ArrayList<Resource>();
-
-		for(SearchResult result : searchListe){
-			if(result.getEntityClassName().equals("Resource")){
-				Resource resource = resourceRepository.find(result.getId());
-				resourceList.add(resource);
-			} 
-			
+	@RequestMapping("/searchallresource")
+	public ModelAndView getAllResource(/*@RequestParam("search") String searchResource*/){
+		List<SearchResult> resultList = searchService.search("mathématique" /*searchResource*/);
+		List<Long> resultid = new ArrayList<Long>();
+		
+		for(SearchResult result : resultList){
+			resultid.add(result.getId()); 
 		}
 		
-		ModelAndView mv = new ModelAndView("search", "resourceList", resourceList);
+		ModelAndView mv = new ModelAndView("search", "resourceList", resourceRepository.findResourcesByIdList(resultid));
+		
+		return mv;
+	}
+	
+	@RequestMapping("/search")
+	public ModelAndView getFirstFiveResource(@RequestParam("search") String searchResource){
+		
+		ModelAndView mv = new ModelAndView("search", "resourceList", searchService.getFirstResources(searchService.search(searchResource), 5));
 		
 		return mv;
 	}
