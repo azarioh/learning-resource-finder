16f1389e4da10beb5ba3e30a08a4c87766ca414a
jaumottej@gmail.com <jaumottej@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-12 14:19:39 +0000

    updated with Competence CRUD & ajax response methods
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@589 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/CompetenceController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/CompetenceController.java
@@ -1,6 +1,5 @@
 package learningresourcefinder.controller;
 
-import learningresourcefinder.model.BaseEntity;
 import learningresourcefinder.model.Competence;
 import learningresourcefinder.repository.CompetenceRepository;
 import learningresourcefinder.security.SecurityContext;
@@ -16,16 +15,16 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CompetenceController extends BaseController<Competence> {
-    @Autowired
-    CompetenceRepository competencerepository; 
 
+	@Autowired CompetenceRepository competenceRepository;
+	
 	@RequestMapping ("/tree")
-	public ModelAndView DisplayCompTree(){
+	public ModelAndView DisplayCompTree (){
 		ModelAndView mv= new ModelAndView("competencetree");
 		return mv; 
 	}
 
-	@RequestMapping({"/competence/test"}) // out of context test of ajax query to add a competence in pgm tree
+	@RequestMapping({"/competence/test"}) // out of context test of ajax query to add/edit/remove a competence in pgm tree
 	public ModelAndView prepareModelAndView() {
 
 		ModelAndView mv = new ModelAndView("testAjaxCompetence", "competence", null);
@@ -34,40 +33,72 @@ public class CompetenceController extends BaseController<Competence> {
 	}
 
 	@RequestMapping(value="/ajax/competenceAddSubmit", method = RequestMethod.POST)
-	public @ResponseBody String ajaxAddCompetence( @RequestParam("name") String nameCompetence, @RequestParam("code") String codeCompetence, @RequestParam("idparent") Long parentIdCompetence){
+	public @ResponseBody String ajaxAddCompetence( @RequestParam("name") String nameCompetence, @RequestParam("code") String codeCompetence, @RequestParam("idparent") Long parentIdCompetence, @RequestParam("description") String descriptionCompetence){
 		
-		if (SecurityContext.canCurrentUserEditCompetence()){
-			Competence parent = em.find(Competence.class, parentIdCompetence);
-			Competence competence = new Competence(codeCompetence,nameCompetence,parent);
-			em.persist(competence);
-			return "OK : name " + nameCompetence  + " code " + codeCompetence + "in parent " + parent.getName();  // We must return something (else the browser thinks it's an error case), but the value is not needed by our javascript code on the browser.
-		}
-		else{
-			return "KO";
-		}
+		//checking
 
+		//parent
+		if (!competenceRepository.getIfParentExist(parentIdCompetence)) {
+			return "La compétence parente n'existe pas. Faites-vous du URL hacking. Si vous êtes parvenu à ce message sans malice, il s'agit d'un bug. parentIdCompetence = "+ parentIdCompetence; 
+		}
+		// required code
+		if (codeCompetence.equals("") || codeCompetence == null) {
+			return "Le code ne peut être vide";
+		}
+		// unique code
+		if (competenceRepository.getIfCodeExist(codeCompetence)) {
+			return "Le code '"+codeCompetence+"' existe déjà dans une autre compétence. Chaque compétence doit avoir un code unique.";
+		}
+		
+		Competence parent = getRequiredEntity(parentIdCompetence); 
+		Competence competence = new Competence(codeCompetence,nameCompetence,parent,descriptionCompetence);
+		SecurityContext.assertCurrentUserMayEditThisCompetence(competence);
+		competence.setName(nameCompetence);
+		competence.setCode(codeCompetence);
+		competence.setDescription(descriptionCompetence);
+		em.persist(competence);
+		return "success";
 
 	}
 
 	@RequestMapping(value="/ajax/competenceRemoveSubmit", method = RequestMethod.POST)
 	public @ResponseBody String ajaxRemoveCompetence( @RequestParam("id") Long idCompetence){
-
-		if (SecurityContext.canCurrentUserEditCompetence()){
-			Competence competence = em.find(Competence.class, idCompetence);
-			em.remove(competence);
-			return "OK : id " + idCompetence;  // We must return something (else the browser thinks it's an error case), but the value is not needed by our javascript code on the browser.
-		}else{
-			return "KO";
-		}
-
 		
+		Competence competence = getRequiredEntity(idCompetence);
+		SecurityContext.assertCurrentUserMayEditThisCompetence(competence);
+		//checking
+		if (competence.getChildrenAndSubChildren().size() == 0){
+			em.remove(competence);
+			return "success";
+		}else{
+			return "La compétence possède une ou plusieurs compétences, veuillez d'abord les supprimer "; // We must return something (else the browser thinks it's an error case), but the value is not needed by our javascript code on the browser.
+		}
 	}
+	
+	@RequestMapping(value="/ajax/competenceEditSubmit", method = RequestMethod.POST)
+	public @ResponseBody String ajaxEditCompetence( @RequestParam("name") String nameCompetence, @RequestParam("code") String codeCompetence, @RequestParam("description") String descriptionCompetence, @RequestParam("id") Long idCompetence){
+		
+		//checking
+		if (competenceRepository.getIfCompetenceCodeExistOrIsCurrentlyBeingEdited(idCompetence,codeCompetence)) {
+			Competence competence = getRequiredDetachedEntity(idCompetence);
+			SecurityContext.assertCurrentUserMayEditThisCompetence(competence);
+			competence.setName(nameCompetence);
+			competence.setCode(codeCompetence);
+			competence.setDescription(descriptionCompetence);
+			em.merge(competence);
+			return "success";
+		}else{
+			return "Le code existe déjà dans une autre compétence"; // We must return something (else the browser thinks it's an error case), but the value is not needed by our javascript code on the browser.
+		}
+ 
+	}
+	
     @RequestMapping(value="/competencemovesubmit")
     public ModelAndView CompetenceMoveSubmit(@RequestParam ("hiddenFieldMoveCompetency") Long idCompetence, @RequestParam("codeField") String codeNewParent ){
         ModelAndView mv= new ModelAndView("competencetree"); 
         
         Competence competence=getRequiredEntity(idCompetence);
-        Competence newParent =   competencerepository.findByCode(codeNewParent);
+        Competence newParent =   competenceRepository.findByCode(codeNewParent);
         if (newParent==null){
             NotificationUtil.addNotificationMessage("Code parent inexistant"); 
             return mv;
@@ -78,9 +109,44 @@ public class CompetenceController extends BaseController<Competence> {
         }
         newParent.getChildren().add(competence);
         competence.setParent(newParent); 
-        competencerepository.merge(competence);
+        competenceRepository.merge(competence);
 
         return mv; 
-}
+    }
+	
+	@RequestMapping(value="/ajax/competenceEditFillFields")
+	public @ResponseBody CompetenceDataHolder competenceEditFillFields( @RequestParam("id") Long idCompetence){ 
+		Competence competence = getRequiredEntity(idCompetence);
+		SecurityContext.assertCurrentUserMayEditThisCompetence(competence);
+		CompetenceDataHolder competenceDH = new CompetenceDataHolder();
+		competenceDH.setCode(competence.getCode());
+		competenceDH.setDescription(competence.getDescription());
+		competenceDH.setName(competence.getName());
+		return competenceDH; 
+	} 
 
+	//we use CompetenceDataHolder because spring cannot perform entities to jSON 	
+	public static class CompetenceDataHolder {
+		private String name;
+		private String code;   // Short identifier that users can use to quickly refer a Competence
+		private String description;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getCode() {
+			return code;
+		}
+		public void setCode(String code) {
+			this.code = code;
+		}
+		public String getDescription() {
+			return description;
+		}
+		public void setDescription(String description) {
+			this.description = description;
+		}
+	}
 }
