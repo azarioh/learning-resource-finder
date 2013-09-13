c184c6c0f3930405778f6ddfddf418b47f64b712
tslock@gmail.com <tslock@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-13 13:37:26 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@617 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/SQL/v0.2-0.3.sql
+++ trunk/LearningResourceFinder/SQL/v0.2-0.3.sql
@@ -6,3 +6,11 @@ INSERT INTO competence(
             updatedby_id, parent_id)
     VALUES (nextval('hibernate_sequence'), null, null, 'Les', null, 'Compétences', null , null
             , null);
+            
+-- Thomas 2013-09-13 15h30
+
+ALTER TABLE resource ADD COLUMN advertising boolean;
+ALTER TABLE resource ADD COLUMN duration integer;
+ALTER TABLE resource ADD COLUMN format character varying(255);
+ALTER TABLE resource ADD COLUMN language character varying(255);
+ALTER TABLE resource ADD COLUMN platform character varying(255);
