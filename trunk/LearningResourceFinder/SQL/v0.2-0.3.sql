e9ae2450028fb0b8c7f6b369606401ef0d5d2f96
jaumottej@gmail.com <jaumottej@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-13 10:55:10 +0000

    add insert script for main competence node, only if necessary
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@607 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/SQL/v0.2-0.3.sql
+++ trunk/LearningResourceFinder/SQL/v0.2-0.3.sql
@@ -1,1 +1,8 @@
 -- v0.2 to v0.3
+
+-- Julien 2013-09-13 13h : If this competence record with no parent is not present in in your LRF DB, InitializeDB batch do this
+INSERT INTO competence(
+            id, createdon, updatedon, code, description, name, createdby_id, 
+            updatedby_id, parent_id)
+    VALUES (nextval('hibernate_sequence'), null, null, 'Les', null, 'Compétences', null , null
+            , null);
