44e71519a154b8251f41c94c1287a246c6d636f7
Thomas.Delizee@gmail.com <Thomas.Delizee@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-13 10:30:05 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@605 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/WebContent/js/int/addImageUrlPlaylist.js
+++ trunk/LearningResourceFinder/WebContent/js/int/addImageUrlPlaylist.js
@@ -4,10 +4,13 @@ jQuery(function($) {
 	    $('#inputUrl').val(''); 
 	  });	  
 	  
-	  // Change radio button if leave by a.close. (to prevent a bug that if we close the dialog and we come back, the radios and the right part are still in sync).
-	  $('a.close').on('click', function() {
+	// Change radio button if leave by a.close. (to prevent a bug that if we close the dialog and we come back, the radios and the right part are still in sync).
+	  $('.closeModal').on('click', function() {
 		 document.getElementById('inputComputer').checked = true; 
+		 document.getElementById('inputUrl').type='hidden';
+		 document.getElementById('inputFile').type='file';
 	  });
+	  
 	  // PAGE : playlistdisplay.jsp
 	  $('.radioUrl').on('change', function() {
 		  document.getElementById('inputFile').type='hidden';
