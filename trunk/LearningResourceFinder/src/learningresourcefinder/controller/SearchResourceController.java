a9062a76edfbb3f594f698387e2de05038033a1f
tslock@gmail.com <tslock@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-16 12:39:41 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@658 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/SearchResourceController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/SearchResourceController.java
@@ -2,7 +2,9 @@ package learningresourcefinder.controller;
 
 import java.util.List;
 
+import learningresourcefinder.model.BaseEntity;
 import learningresourcefinder.model.Resource;
+import learningresourcefinder.repository.ResourceRepository;
 import learningresourcefinder.search.SearchOptions;
 import learningresourcefinder.service.SearchService;
 
@@ -17,25 +19,26 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class SearchResourceController {
 	@Autowired private SearchService searchService;
+	@Autowired private ResourceRepository resourceRepository;
 
 	@RequestMapping("/searchresource")
 	public String searchresource(String searchPhrase, Model model, @RequestParam("search") String searchResource){
-		SearchOptions searchoptions = new SearchOptions();
-		model.addAttribute("searchoptions", searchoptions);
+		SearchOptions searchOptions = new SearchOptions();
+		model.addAttribute("searchOptions", searchOptions);
 		model.addAttribute("natureEnumAllValues", SearchOptions.Nature.values());
-		model.addAttribute("platformsEnumAllValues", SearchOptions.Platforms.values());
-		model.addAttribute("formatEnumAllValues", SearchOptions.Formats.values());
-		model.addAttribute("languagesEnumAllValues", SearchOptions.Languages.values());
-		model.addAttribute("search", "math"); // change math to searchResource
+		model.addAttribute("platformsEnumAllValues", SearchOptions.Platform.values());
+		model.addAttribute("formatEnumAllValues", SearchOptions.Format.values());
+		model.addAttribute("languagesEnumAllValues", SearchOptions.Language.values());
+		model.addAttribute("search", searchResource); // change math to searchResource
 		return "searchresource";
 		
 	}
 	
 	@RequestMapping("/searchresourcesubmit")
 	public String searchresourcesubmit(@ModelAttribute SearchOptions searchOptions, Model model, @RequestParam("search") String searchResource){
-		model.addAttribute("resourcelist", searchService.getFirstEntities(searchService.search(searchResource), 5, Resource.class));
+		List<BaseEntity> ent = resourceRepository.getFilteredEntities(searchService.search(searchResource), 5, Resource.class, searchOptions);
+		model.addAttribute("resourcelist", ent);
 		model.addAttribute("searchOptions", searchOptions);
-		//ModelAndView mv = new ModelAndView("searchresource", "searchOptions", searchOptions);
 		return "searchresource";
 	}
 	
