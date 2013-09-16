755bd0ad9213288cde123c3edf7dca57fcc46adf
tslock@gmail.com <tslock@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-16 12:40:04 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@661 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/repository/ResourceRepository.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/repository/ResourceRepository.java
@@ -1,10 +1,23 @@
 package learningresourcefinder.repository;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.springframework.stereotype.Repository;
+import learningresourcefinder.model.BaseEntity;
 import learningresourcefinder.model.Resource;
 import learningresourcefinder.model.User;
+import learningresourcefinder.search.SearchOptions;
+import learningresourcefinder.search.SearchOptions.Format;
+import learningresourcefinder.search.SearchOptions.Language;
+import learningresourcefinder.search.SearchOptions.Nature;
+import learningresourcefinder.search.SearchOptions.Platform;
+import learningresourcefinder.search.SearchResult;
+
+import org.springframework.stereotype.Repository;
 
 @Repository
 @SuppressWarnings("unchecked")
@@ -30,5 +43,92 @@ public class ResourceRepository extends BaseRepository<Resource>
 		return result;
 	}
 	
+	public List<BaseEntity> getFilteredEntities(List<SearchResult> searchResults, int maxResult, Class<? extends BaseEntity> clazz, SearchOptions searchOptions) {
+		final List<Long> entityIds = new ArrayList<>();
+		
+		for(SearchResult searchResult : searchResults) {
+			if (searchResult.isForClass(clazz)) {
+				entityIds.add(searchResult.getId());
+			}
+			if (entityIds.size() >= maxResult) {
+				break;
+			}	
+		}
 
+		List<BaseEntity> entities = findFilteredEntitiesByIdList(entityIds, clazz, searchOptions);
+		
+		// We need to sort the entities to match the order of the searchResults (the first is supposed to be more relevant) instead of the random ordrer from the DB.
+		Collections.sort(entities, new Comparator<BaseEntity>() {
+			@Override	public int compare(BaseEntity arg0, BaseEntity arg1) {
+				return (new Integer((entityIds.indexOf(arg0.getId())))).compareTo( new Integer(entityIds.indexOf(arg1.getId())));
+			}
+		});
+		
+		return entities;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<BaseEntity> findFilteredEntitiesByIdList(List<Long> idList, Class<? extends BaseEntity> clazz, SearchOptions searchOptions){
+		Map<String, Object> parameters = new HashMap<>();
+		List<String> whereConditions = new ArrayList<>();
+		
+		//// Add Conditions and parameters
+		
+		// Language
+		boolean first = true;
+		String whereCondition = "";
+		for (Language language : searchOptions.getLanguages()) {
+			if (!whereCondition.equals("")) {
+				whereCondition += " OR ";
+			}
+			whereCondition = whereCondition + " r.language='" + language.name() + "' ";
+		}
+		whereConditions.add(whereCondition);
+
+		// Format
+		whereCondition = "";
+		for (Format format : searchOptions.getFormats()) {
+			if (!whereCondition.equals("")) {
+				whereCondition += " OR ";
+			}
+			whereCondition = whereCondition + " r.format='" + format.name() + "' ";
+		}
+		whereConditions.add(whereCondition);
+		
+		
+		// Platform
+		whereCondition = "";
+		for (Platform platform : searchOptions.getPlatforms()) {
+			if (!whereCondition.equals("")) {
+				whereCondition += " OR ";
+			}
+			whereCondition = whereCondition + " r.platform='" + platform.name() + "' ";
+		}
+		whereConditions.add(whereCondition);		
+
+		// Nature
+		whereCondition = "";
+		for (Nature nature : searchOptions.getNature()) {
+			if (!whereCondition.equals("")) {
+				whereCondition += " OR ";
+			}
+			whereCondition = whereCondition + " r.nature='" + nature.name() + "' ";
+		}
+		whereConditions.add(whereCondition);				
+		
+		
+		//// Render the JP-QL statement
+		String whereClause = "";
+		for (String condition : whereConditions) {
+			whereClause += " AND ";
+			whereClause += "(" + condition + ")";
+		}
+		
+
+		
+		List<BaseEntity> result = em.createQuery("SELECT r FROM Resource r WHERE r.id in(:idList) " + whereClause)
+				.setParameter("idList", idList)
+				.getResultList();
+		return result;
+	}
 }
