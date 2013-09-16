6abade15d33f98d7aec5a5b27c6a52078c76de6a
sebastien.dedisse@gmail.com <sebastien.dedisse@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-16 13:06:54 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@662 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/HomeController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/HomeController.java
@@ -1,29 +1,29 @@
 package learningresourcefinder.controller;
 
-import java.security.Security;
+import java.util.List;
 
-import learningresourcefinder.model.PlayList;
-import learningresourcefinder.model.User;
-import learningresourcefinder.security.SecurityContext;
+import learningresourcefinder.model.BaseEntity;
+import learningresourcefinder.model.Cycle;
+import learningresourcefinder.repository.CycleRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
+
 @Controller
-public class HomeController extends BaseController<User>{
- 
+public class HomeController extends BaseController<BaseEntity>{
+    @Autowired CycleRepository cycleRepository;
+    
     @RequestMapping(value={"/", "/home"})
-    private ModelAndView prepareModelAndView() {
-        User u = SecurityContext.getUser();
-        return new ModelAndView("home", "user", u);
+    private ModelAndView home() {
+        ModelAndView mv = new ModelAndView("cyclelist"); 
+        List<Cycle> list=cycleRepository.findAllCycles();
+        mv.addObject("cyclelist", list);
+         
+        return mv;
     }
     
-    @RequestMapping(value={"/test"})
-    private ModelAndView prepareModelAndViewtest() {
-        User u = SecurityContext.getUser();
-        return new ModelAndView("test", "user", u);
-    }
 }
