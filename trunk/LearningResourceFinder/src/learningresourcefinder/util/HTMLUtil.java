779062db0a0ad8f99f9eee4dee1c50185b15781f
isyapan.cz@gmail.com <isyapan.cz@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-18 07:44:31 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@686 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/util/HTMLUtil.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/util/HTMLUtil.java
@@ -1,5 +1,6 @@
 package learningresourcefinder.util;
 
+import learningresourcefinder.model.Competence;
 import learningresourcefinder.model.User;
 
 import org.jsoup.Jsoup;
@@ -7,53 +8,54 @@ import org.jsoup.safety.Whitelist;
 
 
 public class HTMLUtil {
-	
+
 
 	private static Whitelist whiteList = Whitelist.basic().addTags("span").addAttributes(":all", "style");
-	
 
-    
+
+
 	/**
-     * Remove as much HTML as possible.
-     * */
-    public static String removeHmlTags(String textToFormat) {
-        String result = textToFormat.replaceAll("\\<(.|\n)*?>", "");
-        return result;
-    }
-    
- 
+	 * Remove as much HTML as possible.
+	 * */
+	public static String removeHmlTags(String textToFormat) {
+		String result = textToFormat.replaceAll("\\<(.|\n)*?>", "");
+		return result;
+	}
+
+
 
 	public static boolean isHtmlSecure(String htmlToCheck) {
-    	//sometimes controller send null parameter or empty so it isn't unsecure...
-    	if ( htmlToCheck == null || htmlToCheck.isEmpty() ) {
-    		return true;
-    	} 
-    	return Jsoup.isValid(htmlToCheck, whiteList);
-    	
-    }
-	
+		//sometimes controller send null parameter or empty so it isn't unsecure...
+		if ( htmlToCheck == null || htmlToCheck.isEmpty() ) {
+			return true;
+		} 
+		return Jsoup.isValid(htmlToCheck, whiteList);
+
+	}
+
 	/**
 	 * check if the entry contains character who can cause buggy html (< , > , " ,  ')
 	 * @return a String containing forbidden char or null
 	 */
 	public static String getContainedForbiddenHtmlCharacter(String htmlToCheck) {
-        String result = new String();
-        String[] forbiddenChar = { "<" , ">" , "\"" ,  "'" };
-        
-        for( String ch : forbiddenChar ) {
-            if( htmlToCheck.contains(ch) ) {
-                result += ch;
-            }
-        }
-	    
-	    return result.isEmpty()?null:result;
+		String result = new String();
+		String[] forbiddenChar = { "<" , ">" , "\"" ,  "'" };
+
+		for( String ch : forbiddenChar ) {
+			if( htmlToCheck.contains(ch) ) {
+				result += ch;
+			}
+		}
+
+		return result.isEmpty()?null:result;
 	}
 
-
-	
-	
 	public static String getUserPageLink(User user){
-	    
-	    return "<a href='/user/"+user.getUserName()+"'>"+user.getFullName()+"</a>";
+
+		return "<a href='/user/"+user.getUserName()+"'>"+user.getFullName()+"</a>";
+	}
+
+	public static String getCompetencePath(Competence competence) {
+		return competence + "/ ";
 	}
 }
