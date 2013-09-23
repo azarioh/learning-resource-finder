8b49ab4791161d4e185e119d072b8d09d570d3b8
isyapan.cz@gmail.com <isyapan.cz@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-23 07:23:42 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@721 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/ComingSoonController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/ComingSoonController.java
@@ -1,7 +1,9 @@
 package learningresourcefinder.controller;
 
 import learningresourcefinder.model.ComingSoonMail;
+import learningresourcefinder.repository.SendMailRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,13 +14,15 @@ import org.springframework.web.context.request.WebRequest;
 @Controller
 public class ComingSoonController extends BaseController<ComingSoonMail> {
 
+	@Autowired SendMailRepository sendMailRepository;
+	
 	@RequestMapping(value="/ajax/addMailOnTable", method = RequestMethod.POST)
-	public @ResponseBody String addMailOnTable( @RequestParam("mail") String addresemail){
-		
+	public @ResponseBody String  addMailOnTable( @RequestParam("mail") String addresemail){
+	
 		ComingSoonMail comingSoonMail = new ComingSoonMail();	
 		comingSoonMail.setEmail(addresemail);
 		em.persist(comingSoonMail);
-		
+		em.getTransaction().commit();
 		return "OK";  // We must return something (else the browser thinks it's an error case), but the value is not needed by our javascript code on the browser.
 	}
 }
