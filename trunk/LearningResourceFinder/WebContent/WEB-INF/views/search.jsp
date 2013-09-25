ed74a5519d058c0da59f58caa64822a4bb54c525
ouz.nour@gmail.com <ouz.nour@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-25 07:09:53 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@739 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/WebContent/WEB-INF/views/search.jsp
+++ trunk/LearningResourceFinder/WebContent/WEB-INF/views/search.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
 <%@ taglib uri='http://java.sun.com/jsp/jstl/core' prefix='c'%>
 <%@ taglib tagdir="/WEB-INF/tags/lrftag/" prefix="lrftag"%>
+ <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -48,7 +49,7 @@
 	<br />
 	<br />
 	<br />
-	<a href="searchresource?search=${searchValue}&page=1">More</a>
+	<a href="searchresource?search=${searchValue}">${fn:length(searchResultsMore)}</a>
 	<br />
 	<br />
 	<br />
--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/SearchResourceController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/SearchResourceController.java
@@ -3,9 +3,9 @@ package learningresourcefinder.controller;
 import java.util.List;
 
 import learningresourcefinder.model.BaseEntity;
-import learningresourcefinder.model.Resource;
 import learningresourcefinder.repository.ResourceRepository;
 import learningresourcefinder.search.SearchOptions;
+import learningresourcefinder.search.SearchResult;
 import learningresourcefinder.service.SearchService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +13,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+
+
 @Controller
 public class SearchResourceController {
 	@Autowired private SearchService searchService;
 	@Autowired private ResourceRepository resourceRepository;
 
 	@RequestMapping("/searchresource")
-	public String searchresource(String searchPhrase, Model model, @RequestParam("search") String searchResource, @RequestParam("page") int page){
+	public String searchresource(String searchPhrase, Model model, @RequestParam("search") String searchResource, @RequestParam(value="page", required=false) Integer page){
+		if (page == null) page=1;
 		SearchOptions searchOptions = new SearchOptions();
 		model.addAttribute("searchOptions", searchOptions);
 		model.addAttribute("natureEnumAllValues", SearchOptions.Nature.values());
@@ -31,17 +32,20 @@ public class SearchResourceController {
 		model.addAttribute("languagesEnumAllValues", SearchOptions.Language.values());
 		model.addAttribute("search", searchResource);
 		
-		List<BaseEntity> entities = resourceRepository.getFilteredEntities(searchService.search(searchResource), page, searchOptions);
-		int numberOfResourceFound = searchService.search(searchResource).size();
+		List<SearchResult> searchResults = searchService.search(searchResource);
+		List<BaseEntity> entities = resourceRepository.getFilteredEntities(searchResults, page, searchOptions);
+		int numberOfResourceFound = searchResults.size();
 		
-		model.addAttribute("numberResource", numberOfResourceFound);
+		model.addAttribute("numberResource", numberOfResourceFound); // FIXME replace by EL
+		
 		model.addAttribute("resourcelist", entities);
 		return "searchresource";
 		
 	}
 	
 	@RequestMapping("/searchresourcesubmit")
-	public String searchresourcesubmit(@ModelAttribute SearchOptions searchOptions, Model model, @RequestParam("search") String searchResource, @RequestParam("page") int page){
+	public String searchresourcesubmit(@ModelAttribute SearchOptions searchOptions, Model model, @RequestParam("search") String searchResource, @RequestParam(value="page", required=false) Integer page){
+		if (page == null) page=1;
 		List<BaseEntity> entities = resourceRepository.getFilteredEntities(searchService.search(searchResource), page, searchOptions);
 		model.addAttribute("resourcelist", entities);
 		model.addAttribute("searchOptions", searchOptions);
