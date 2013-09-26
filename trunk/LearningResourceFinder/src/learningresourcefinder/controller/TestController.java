3be1ad6b4278c0335eda43e79e561ebf865a39fc
ibrahim.yock@gmail.com <ibrahim.yock@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-26 08:53:11 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@750 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/TestController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/TestController.java
@@ -1,9 +1,8 @@
 package learningresourcefinder.controller;
 
-import java.util.List;
-
 import learningresourcefinder.model.BaseEntity;
-import learningresourcefinder.model.Cycle;
+import learningresourcefinder.model.Competence;
+import learningresourcefinder.repository.CompetenceRepository;
 import learningresourcefinder.repository.CycleRepository;
 import learningresourcefinder.util.NotificationUtil;
 
@@ -17,16 +16,17 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class TestController extends BaseController<BaseEntity>{
     @Autowired CycleRepository cycleRepository;
+    @Autowired CompetenceRepository competenceRepository;
     
     @RequestMapping("/test")
-    private ModelAndView test() {
-         
-        return new ModelAndView("test");
+    public ModelAndView test() {
+        Competence competence = competenceRepository.find(12L);
+        return new ModelAndView("test", "competence", competence);
     }
     
 
     @RequestMapping("/test/addMsg")
-    private ModelAndView addMsg() {
+    public ModelAndView addMsg() {
         NotificationUtil.addNotificationMessage("Ceci est un message.");
         return new ModelAndView("test");
     }
