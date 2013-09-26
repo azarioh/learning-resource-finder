a7bc338ed86306641e4f942005dceee3aff4cc48
ibrahim.yock@gmail.com <ibrahim.yock@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-26 08:53:23 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@751 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/tag/CompetencePathTag.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/tag/CompetencePathTag.java
@@ -1,13 +1,17 @@
 package learningresourcefinder.tag;
 
 
-import javax.servlet.jsp.JspException;
+import java.io.IOException;
+
+import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
+
 import learningresourcefinder.model.Competence;
 
-public class CompetencePathTag extends SimpleTagSupport{
-	
+public class CompetencePathTag extends SimpleTagSupport {
+
 	private Competence competence;
+	String result;
 
 	public Competence getCompetence() {
 		return competence;
@@ -16,11 +20,23 @@ public class CompetencePathTag extends SimpleTagSupport{
 	public void setCompetence(Competence competence) {
 		this.competence = competence;
 	}
-	
+
 	@Override
-	public void doTag() {
-		String result;
-	
+	public void doTag() throws IOException {
+		while (competence != null) { 
+			if (result == null) {
+				result =  competence.getFullName();
+			} else {
+				result =  competence.getFullName() + " / " + result;
+			}
+			competence = competence.getParent();
+		}
+		if (result == null) {
+			throw new RuntimeException("Bug - defensive coding: result should not be null here");
+		}
+		
+		JspWriter out = this.getJspContext().getOut();
+		out.write(result);
 	}
 
 }
