4f7b4e5233e5a97537eaaa96913e59832181f55b
lionel.gosseries@gmail.com <lionel.gosseries@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-26 07:21:28 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@744 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/tag/CompetenceTreeTag.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/tag/CompetenceTreeTag.java
@@ -8,34 +8,23 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
 import learningresourcefinder.model.Competence;
 import learningresourcefinder.repository.CompetenceRepository;
 import learningresourcefinder.repository.CycleRepository;
-import learningresourcefinder.util.CompetencesTreeVisitorImpl;
+import learningresourcefinder.util.CompetencesTreeVisitorHtml;
 import learningresourcefinder.util.CompetencesTreeWalker;
 import learningresourcefinder.web.ContextUtil;
 
 public class CompetenceTreeTag extends SimpleTagSupport {
-	
-	Competence root;
-	
     @Override 
     public void doTag()  {
         try {
             JspWriter out = this.getJspContext().getOut();
             CompetenceRepository competenceRepository= ContextUtil.getSpringBean(CompetenceRepository.class);
             CycleRepository cycleRepository = ContextUtil.getSpringBean(CycleRepository.class);
-            CompetencesTreeVisitorImpl ctv= new CompetencesTreeVisitorImpl();  
-            CompetencesTreeWalker ctw = new CompetencesTreeWalker(ctv, root, competenceRepository, cycleRepository);
+            CompetencesTreeVisitorHtml ctv= new CompetencesTreeVisitorHtml();  
+            CompetencesTreeWalker ctw = new CompetencesTreeWalker(competenceRepository,ctv,cycleRepository);
             ctw.walk();
             out.write(ctv.getHtmlResult());
         } catch (IOException e) {
             throw new RuntimeException(e);
         } 
     }
-
-	public Competence getRoot() {
-		return root;
-	}
-
-	public void setRoot(Competence root) {
-		this.root = root;
-	}
 }
