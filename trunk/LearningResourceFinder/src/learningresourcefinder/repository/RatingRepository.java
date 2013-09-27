8bca7c3737c7891d70adb97e2c5e5cd7715bc00f
Thomas.Delizee@gmail.com <Thomas.Delizee@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-27 14:38:02 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@800 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/repository/RatingRepository.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/repository/RatingRepository.java
@@ -1,5 +1,7 @@
 package learningresourcefinder.repository;
 
+import java.util.List;
+
 import learningresourcefinder.model.Rating;
 import learningresourcefinder.model.Resource;
 import learningresourcefinder.model.User;
@@ -15,6 +17,15 @@ public class RatingRepository extends BaseRepository<Rating>{
 				.setParameter("resource", resource)
 				.getSingleResult();
 		
+	}
+	
+	public List<Rating> listRating(List<Resource> listResource, User user) {
+		List<Rating> listRating = (List<Rating>) em.createQuery("SELECT ra.score FROM Rating ra WHERE ra.user=:user AND ra.resource in (:listResource)")
+				.setParameter("user", user)
+				.setParameter("listResource", listResource)
+				.getResultList();
+		
+		return listRating;
 	}
 	
 	public Rating getRatingForUserAndResource(Resource resource, User user){
