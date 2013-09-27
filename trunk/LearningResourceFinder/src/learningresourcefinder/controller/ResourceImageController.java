e82b54205ec3aa9ab77da67ab5be360e698fbd17
Thomas.Delizee@gmail.com <Thomas.Delizee@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-27 09:49:30 +0000

    Delete import useless
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@781 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/ResourceImageController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/ResourceImageController.java
@@ -5,8 +5,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.imageio.ImageIO;
 
@@ -28,12 +26,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
 
 
 
