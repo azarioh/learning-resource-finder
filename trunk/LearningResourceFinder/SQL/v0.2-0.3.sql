5d63343968f800c8235bc0d633c6723c5236a2c6
jaumottej@gmail.com <jaumottej@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-27 09:10:12 +0000

    add Rating creation sql script
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@778 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/SQL/v0.2-0.3.sql
+++ trunk/LearningResourceFinder/SQL/v0.2-0.3.sql
@@ -31,3 +31,38 @@ ALTER TABLE resource ADD COLUMN nature character varying(255);
 -- Emile 2013-09-27
 alter table resource 
         add column score float8; 
+        
+-- Julien 2013-09-27
+        
+        create table rating (
+        id int8 not null,
+        createdon timestamp,
+        updatedon timestamp,
+        score float8,
+        createdby_id int8,
+        updatedby_id int8,
+        resource_id int8,
+        user_id int8,
+        primary key (id)
+    );
+
+    alter table rating 
+        add constraint fk_echhg31lrj7xqwilu1quu8vo3 
+        foreign key (createdby_id) 
+        references users;
+
+    alter table rating 
+        add constraint fk_kk591y4orkmfaaf90sw579f0h 
+        foreign key (updatedby_id) 
+        references users;
+
+    alter table rating 
+        add constraint fk_4qubpqyyepfhty50lwce7wa82 
+        foreign key (resource_id) 
+        references resource;
+
+    alter table rating 
+        add constraint fk_e04g3baa3n3ufi0m3nrmm17r6 
+        foreign key (user_id) 
+        references users;
+        
