e05800b474613649ece2eecacb3baef471f523e7
sebastien.dedisse@gmail.com <sebastien.dedisse@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-30 16:18:04 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@836 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/ResourceEditController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/ResourceEditController.java
@@ -1,10 +1,13 @@
 package learningresourcefinder.controller;
 
+import learningresourcefinder.exception.InvalidUrlException;
 import learningresourcefinder.model.Resource;
 import learningresourcefinder.model.UrlResource;
 import learningresourcefinder.repository.ResourceRepository;
 import learningresourcefinder.repository.UrlResourceRepository;
-import learningresourcefinder.search.SearchOptions;
+import learningresourcefinder.search.SearchOptions.Format;
+import learningresourcefinder.search.SearchOptions.Language;
+import learningresourcefinder.search.SearchOptions.Platform;
 import learningresourcefinder.security.SecurityContext;
 import learningresourcefinder.service.IndexManagerService;
 import learningresourcefinder.web.Slugify;
@@ -12,8 +15,6 @@ import learningresourcefinder.web.UrlUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,7 +38,8 @@ public class ResourceEditController extends BaseController<Resource> {
     
     @RequestMapping("/ajax/resourceaddsubmit1")
 //	public @ResponseBody String resourceAddSubmit() {
-	public @ResponseBody MessageAndId resourceAddSubmit(@RequestParam("url") String url, @RequestParam("title") String title, @ModelAttribute SearchOptions searchoptions ) {
+	public @ResponseBody MessageAndId resourceAddSubmit(@RequestParam(value="url", required=false) String url, @RequestParam(value="title",required=false) String title,
+	        @RequestParam(value="format",required=false) Format format, @RequestParam(value="platform",required=false) Platform platform ) {
         SecurityContext.assertUserIsLoggedIn();
    
         Resource resource = new Resource();
@@ -45,6 +47,8 @@ public class ResourceEditController extends BaseController<Resource> {
         resource.setName(title);
         String slug = Slugify.slugify(resource.getName());
         resource.setSlug(slug);
+        resource.setFormat(format);
+        resource.setPlatform(platform);
 
         UrlResource urlResource = new UrlResource();
         urlResource.setName(title);
@@ -52,7 +56,7 @@ public class ResourceEditController extends BaseController<Resource> {
         urlResource.setResource(resource);
         resource.getUrlResources().add(urlResource);
         
-        resource.setFormat(searchoptions.getFormat().get(0));
+        //resource.setFormat(searchoptions.getFormat().get(0));
         
 
         urlResourceRepository.persist(urlResource);
@@ -61,6 +65,27 @@ public class ResourceEditController extends BaseController<Resource> {
         indexManager.add(resource);
         
         return new MessageAndId(resource.getId(), "ressource ajoutée - <a href="+UrlUtil.getRelativeUrlToResourceDisplay(resource)+">Afficher</a>");
+    }
+    
+    @RequestMapping("ajax/resourceaddsubmit2")
+    public @ResponseBody MessageAndId resourceAddSubmit2(@RequestParam(value="description",required=false)String description,
+            @RequestParam(value="idresource",required=false) Long id,
+            @RequestParam(value="language",required=false) Language language,
+            @RequestParam(value="advertising",required=false) Boolean advertising,
+            @RequestParam(value="maxDuration",required=false)int duration){
+        if(id==null){
+             throw new InvalidUrlException("Missing resource id.");
+        }
+        Resource resource=getRequiredEntity(id);
+        resource.setLanguage(language);
+        resource.setDescription(description);
+        resource.setAdvertising(advertising);
+        resource.setDuration(duration); 
+        
+        resourceRepository.merge(resource); 
+       
+        
+        return new MessageAndId(id,"ressource updaté");
     }
 
     
