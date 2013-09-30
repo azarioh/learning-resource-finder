942bc239d556ac4ebf3002a5a3015973192eb14a
isyapan.cz@gmail.com <isyapan.cz@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-09-30 13:15:08 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@827 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/util/CompetencesTreeVisitorImpl.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/util/CompetencesTreeVisitorImpl.java
@@ -25,57 +25,61 @@ public class CompetencesTreeVisitorImpl implements CompetencesTreeVisitor {
         String labelCycle;
 
 
-        htmlResult += "<li>" + " " + competence.getCode() + " " + competence.getName() + " ";
+        htmlResult += "<li>" + " " + competence.getCode() + " - " + competence.getName() + " ";
 
         if(SecurityContext.isUserHasPrivilege(Privilege.MANAGE_COMPETENCE)){
+    		htmlResult += "<small>";
         	if (competence.getParent() != null){ // not the main node
+
         		///////////// Assign Cycle
         		/// First, we check if we can display the dropdown to enable the user selecting another cycle for that competence.
         		boolean canDisplayCycleDropdown = true;  // If the parent has a cycle, the user should not set cycles to kids (it would be a non-sense to select another cycle than the parent).
-        			// Has a parent a defined cycle?
-        			Competence parent = competence.getParent();
-        			while(parent != null) {
-        				if (parent.getCycle() != null) {
-        					canDisplayCycleDropdown = false;
-        					break;
-        				}
-        				parent = parent.getParent();
+        		// Has a parent a defined cycle?
+        		Competence parent = competence.getParent();
+        		while(parent != null) {
+        			if (parent.getCycle() != null) {
+        				canDisplayCycleDropdown = false;
+        				break;
         			}
-        		
-        		// second, we generate the html
-        		if (canDisplayCycleDropdown) {
-        		    if (competence.getCycle() != null) {
-        		        labelCycle = competence.getCycle().getName();
-        		    } else {
-        		        labelCycle = "Allouer un cycle";
-        		    }
-        		    htmlResult +="<span class=\"dropdown\">"
-        		            +"<a id=\"CP-"+ competence.getId() +"\" role=\"button\" data-toggle=\"dropdown\" data-target=\"#\" href=\"/page.html\" value=" + labelCycle + ">"
-        		            + labelCycle + "<span class=\"caret\"></span>"
-        		            +"</a>"
-        		            +"<ul class=\"dropdown-menu\" role=\"menu\" aria-labelledby=\"dLabel\">";
-
-        		    for(Cycle c : lc){
-        		        htmlResult +="<li role=\"presentation\"><a role=\"menuitem\" tabindex=\"-1\" href=\"/competencetree\" id=" + "CY-" +c.getId() + "CP-"+ competence.getId() +"" +">"+c.getName()+"</a></li>";
-        		    }
-        		    htmlResult += "</ul>"
-        		            +"</span>";
-        		} else if (competence.getCycle() != null) { // Display non modifiable name.
-        		    htmlResult += competence.getCycle().getName();
+        			parent = parent.getParent();
         		}
 
-        		
-        		
+        		// second, we generate the html
+        		if (canDisplayCycleDropdown) {
+        			if (competence.getCycle() != null) {
+        				labelCycle = competence.getCycle().getName();
+        			} else {
+        				labelCycle = "Allouer un cycle";
+        			}
+        			htmlResult +="<span class=\"dropdown\">"
+        					+"<a id=\"CP-"+ competence.getId() +"\" role=\"button\" data-toggle=\"dropdown\" data-target=\"#\" href=\"/page.html\" value=" + labelCycle + ">"
+        					+ labelCycle + "<span class=\"caret\"></span>"
+        					+"</a>"
+        					+"<ul class=\"dropdown-menu\" role=\"menu\" aria-labelledby=\"dLabel\">";
+
+        			for(Cycle c : lc){
+        				htmlResult +="<li role=\"presentation\"><a role=\"menuitem\" tabindex=\"-1\" href=\"/competencetree\" id=" + "CY-" +c.getId() + "CP-"+ competence.getId() +"" +">"+c.getName()+"</a></li>";
+        			}
+        			htmlResult += "</ul>"
+        					+"</span>";
+        		} else if (competence.getCycle() != null) { // Display non modifiable name.
+        			htmlResult += competence.getCycle().getName();
+        		}
+
+
+
         		/////////// Move node
         		// Main node hasn't parent, it must just be take like parent and only "Add" option is permit
-        		htmlResult += "<a href=\"#\" " + "id='D-" + id + "'> Déplacer</a>";
+        		htmlResult += " <a href=\"#\" " + "id='D-" + id + "'>Déplacer</a>";
         	}
 
         	//////////////// Add, Edit, Delete node 
-        	htmlResult += "<a href=\"#\" " + "id=" + "\"" + "A-" + id + "\"" + ">" + " Ajouter" + "</a>";
-        	if (competence.getParent() != null){ htmlResult +="<a href=\"#\" " + "id=" + "\"" +  "E-" +id + "\"" + ">"+ " Éditer" +"</a>"
-        			+"<a href=\"#\" " + "id='R-" + id + "'>" + " Supprimer" + "</a>";
+        	htmlResult += " <a href=\"#\" " + "id=" + "\"" + "A-" + id + "\"" + ">" + "Ajouter" + "</a>";
+        	if (competence.getParent() != null){ htmlResult +=" <a href=\"#\" " + "id=" + "\"" +  "E-" +id + "\"" + ">"+ "Éditer" +"</a>"
+        			+" <a href=\"#\" " + "id='R-" + id + "'>" + "Supprimer" + "</a>";
         	} 
+
+        	htmlResult += "</small>";
 
         }
 	}
@@ -83,25 +87,25 @@ public class CompetencesTreeVisitorImpl implements CompetencesTreeVisitor {
 	@Override
 	public void endCompetence(Competence competence) {
 		htmlResult +="</a>";
-        htmlResult += "</li>";
+		htmlResult += "</li>";
 
-    }
+	}
 
-    @Override
-    public void beforeChildren(int recurtionLevel) {
-        htmlResult += "<ul>";
+	@Override
+	public void beforeChildren(int recurtionLevel) {
+		htmlResult += "<ul>";
 
-    }
+	}
 
-    @Override
-    public void afterChildren() {
-        htmlResult += "</ul>";
+	@Override
+	public void afterChildren() {
+		htmlResult += "</ul>";
 
-    }
+	}
 
-    public String getHtmlResult() {
-        return htmlResult;
-    }
+	public String getHtmlResult() {
+		return htmlResult;
+	}
 
 
 }
