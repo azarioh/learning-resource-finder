aaa81db5e29b5c0cc811e02ef6a77f453c6d6f04
Thomas.Delizee@gmail.com <Thomas.Delizee@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-10-09 14:21:30 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@962 59821364-4b7c-4859-2266-30ef78c0912a

---
+++ trunk/LearningResourceFinder/src/learningresourcefinder/repository/DiscussionRepository.java
@@ -0,0 +1,14 @@
+package learningresourcefinder.repository;
+
+import java.util.List;
+
+import learningresourcefinder.model.Discussion;
+import learningresourcefinder.model.User;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+@SuppressWarnings("unchecked")
+public class DiscussionRepository extends BaseRepository<Discussion> {
+
+}
