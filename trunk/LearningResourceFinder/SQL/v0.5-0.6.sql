140536cfbe32063cdc7f74cfe418f52480322b08
john.rizzo.be@gmail.com <john.rizzo.be@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-10-10 06:21:35 +0000

    SQL
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@968 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/SQL/v0.5-0.6.sql
+++ trunk/LearningResourceFinder/SQL/v0.5-0.6.sql
@@ -1,13 +1,24 @@
 
-/*                        
-                             ////
+                       
+--                           ////
 -- 2013-10-09 -- Ahmed -- (£|°_°|£)
-*/
+
+
     alter table users 
         add column accountlevel int4 not null default 1;
 
     alter table users 
         add column userprogresspoints int4 not null default 0;
         
-        
---*******************************************End of SQL********************************************
+-- DEPLOYED - DO NOT USE THIS FILE ANYMORE
+-- DEPLOYED - DO NOT USE THIS FILE ANYMORE
+-- DEPLOYED - DO NOT USE THIS FILE ANYMORE
+-- DEPLOYED - DO NOT USE THIS FILE ANYMORE
+-- DEPLOYED - DO NOT USE THIS FILE ANYMORE
+-- DEPLOYED - DO NOT USE THIS FILE ANYMORE
+-- DEPLOYED - DO NOT USE THIS FILE ANYMORE
+-- DEPLOYED - DO NOT USE THIS FILE ANYMORE
+-- DEPLOYED - DO NOT USE THIS FILE ANYMORE
+-- DEPLOYED - DO NOT USE THIS FILE ANYMORE
+-- DEPLOYED - DO NOT USE THIS FILE ANYMORE
+-- DEPLOYED - DO NOT USE THIS FILE ANYMORE
