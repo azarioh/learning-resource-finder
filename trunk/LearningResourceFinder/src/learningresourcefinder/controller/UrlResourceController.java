f07d308da50717f44a9851a6627cb85407f096fd
emilekane@gmail.com <emilekane@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-10-14 09:43:53 +0000

    urlResource
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@1005 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/UrlResourceController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/UrlResourceController.java
@@ -1,77 +1,18 @@
 package learningresourcefinder.controller;
 
-import javax.validation.Valid;
-
-
-import learningresourcefinder.exception.InvalidUrlException;
-import learningresourcefinder.model.Resource;
 import learningresourcefinder.model.UrlResource;
 import learningresourcefinder.repository.ResourceRepository;
 import learningresourcefinder.repository.UrlResourceRepository;
-import learningresourcefinder.util.NotificationUtil;
-import learningresourcefinder.util.NotificationUtil.Status;
-import learningresourcefinder.web.UrlUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-
 
 @Controller
 public class UrlResourceController extends BaseController<UrlResource> {
-	
-	@Autowired ResourceRepository resourceRepository;
-	@Autowired UrlResourceRepository urlResourceRepository;
-	
-	@RequestMapping("/removeurlresource")
-	public ModelAndView removeResource(@RequestParam("id") long id) {
-		UrlResource urlResource = getRequiredEntity(id); 
-		Resource resource = urlResource.getResource();
 
-		resource.getUrlResources().remove(id);
-		em.remove(urlResource); 
-	
-		return new ModelAndView ("redirect:"+UrlUtil.getRelativeUrlToResourceDisplay(resource));
-	}
-	
-	
-	@RequestMapping("/urlresourceeditsubmit")
-	public String urlSubmit(@RequestParam(value="urlresourceid", required=false) Long urlResourceId, @RequestParam("resourceid") long resourceId, @RequestParam("name") String name, @RequestParam("url") String url) 
-	{
-		// TODO: voir si url est valide (sinon ajouter une erreur sur result)
-	    
-	    Resource resource = resourceRepository.find(resourceId);
-		if(true) {   // No errors
-		    UrlResource urlResource;
-		    if (urlResourceId != null) {
-		        urlResource = urlResourceRepository.find(urlResourceId);
-		    } else {
-		        urlResource = new UrlResource();
-	            if (resource == null) {
-	                throw new InvalidUrlException("Invalid resource id " + resourceId);
-	            }
-		        urlResource.setResource(resource);
-		    }
-		    
-			urlResource.setName(name);
-			urlResource.setUrl(url);
-			
-			if (urlResource.getId() == null) {  // Create
-			    urlResourceRepository.persist(urlResource);
-	            NotificationUtil.addNotificationMessage("Url ajoutée avec succès", Status.SUCCESS);         
-			} else {  // Edit
-			    urlResourceRepository.merge(urlResource);
-	            NotificationUtil.addNotificationMessage("Url modifiée avec succès", Status.SUCCESS);         
-			}
-			
-			
-		} else {
-		    NotificationUtil.addNotificationMessage("Erreur sur un des champs. Url non sauvée.", Status.ERROR);
-		}
-		
-	    return ("redirect:"+UrlUtil.getRelativeUrlToResourceDisplay(resource));
-	}
+    @Autowired
+    ResourceRepository resourceRepository;
+    @Autowired
+    UrlResourceRepository urlResourceRepository;
+
 }
