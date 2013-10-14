c026f894c4cffeb83c26279f6ddc8cbd760042f6
emilekane@gmail.com <emilekane@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-10-14 09:42:56 +0000

    resourcedisplay
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@1003 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/ResourceDisplayController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/ResourceDisplayController.java
@@ -1,8 +1,11 @@
 package learningresourcefinder.controller;
 
+import learningresourcefinder.exception.InvalidUrlException;
 import learningresourcefinder.model.Resource;
+import learningresourcefinder.model.UrlResource;
 import learningresourcefinder.model.User;
 import learningresourcefinder.repository.ResourceRepository;
+import learningresourcefinder.repository.UrlResourceRepository;
 import learningresourcefinder.search.SearchOptions;
 import learningresourcefinder.search.SearchOptions.Platform;
 import learningresourcefinder.security.SecurityContext;
@@ -10,7 +13,10 @@ import learningresourcefinder.service.IndexManagerService;
 import learningresourcefinder.service.LevelService;
 import learningresourcefinder.util.Action;
 import learningresourcefinder.util.EnumUtil;
+import learningresourcefinder.util.NotificationUtil;
+import learningresourcefinder.util.NotificationUtil.Status;
 import learningresourcefinder.web.Slugify;
+import learningresourcefinder.web.UrlUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,6 +30,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ResourceDisplayController extends BaseController<Resource> {
     @Autowired ResourceRepository resourceRepository;
+    @Autowired UrlResourceRepository urlResourceRepository;
     @Autowired IndexManagerService indexManager;
     @Autowired LevelService levelService;
     
@@ -91,6 +98,54 @@ public class ResourceDisplayController extends BaseController<Resource> {
 		mv.addObject("dataEnumPlatform",dataEnumPlatform );
 	}
 
+    
+    @RequestMapping("/removeurlresource")
+    public ModelAndView removeResource(@RequestParam("id") long id) {
+        UrlResource urlResource = (UrlResource)getRequiredEntity(id, UrlResource.class); 
+        Resource resource = urlResource.getResource();
 
+        resource.getUrlResources().remove(id);
+        em.remove(urlResource); 
+        NotificationUtil.addNotificationMessage("Url supprimée avec succès.<br/>"+urlResource.getUrl(),Status.SUCCESS);
+        return new ModelAndView ("redirect:"+UrlUtil.getRelativeUrlToResourceDisplay(resource));
+    }
+    
+    
+    @RequestMapping("/urlresourceeditsubmit")
+    public String urlSubmit(@RequestParam(value="urlresourceid", required=false) Long urlResourceId, @RequestParam("resourceid") long resourceId, @RequestParam("name") String name, @RequestParam("url") String url) 
+    {
+        // TODO: voir si url est valide (sinon ajouter une erreur sur result)
+        
+        Resource resource = resourceRepository.find(resourceId);
+        if(true) {   // No errors
+            UrlResource urlResource;
+            if (urlResourceId != null) {
+                urlResource = urlResourceRepository.find(urlResourceId);
+            } else {
+                urlResource = new UrlResource();
+                if (resource == null) {
+                    throw new InvalidUrlException("Invalid resource id " + resourceId);
+                }
+                urlResource.setResource(resource);
+            }
+            
+            urlResource.setName(name);
+            urlResource.setUrl(url);
+            
+            if (urlResource.getId() == null) {  // Create
+                urlResourceRepository.persist(urlResource);
+                NotificationUtil.addNotificationMessage("Url ajoutée avec succès", Status.SUCCESS);         
+            } else {  // Edit
+                urlResourceRepository.merge(urlResource);
+                NotificationUtil.addNotificationMessage("Url modifiée avec succès", Status.SUCCESS);         
+            }
+            
+            
+        } else {
+            NotificationUtil.addNotificationMessage("Erreur sur un des champs. Url non sauvée.", Status.ERROR);
+        }
+        
+        return ("redirect:"+UrlUtil.getRelativeUrlToResourceDisplay(resource));
+    }
 
 }
