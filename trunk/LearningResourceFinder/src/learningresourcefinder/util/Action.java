0666a33bc37f32e7efed5403e434206d5a7fbeb0
isyapan.cz@gmail.com <isyapan.cz@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-10-14 08:16:05 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@998 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/util/Action.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/util/Action.java
@@ -5,9 +5,17 @@ import learningresourcefinder.model.User.Level;
 public enum Action {
 	
 		VOTE(10,Level.LEVEL_2, "Voter sur les ressources"),
-		ADD_RESOURCES(20,Level.LEVEL_2,"Ajouter des ressources"),
-		EDIT_RESOURCES(20,Level.LEVEL_3,"Editer des ressources"),
-		REMOVE_RESOURCES(30,Level.LEVEL_3,"Supprimer des ressources");
+		ADD_RESOURCE(2,Level.LEVEL_2,"Ajouter une ressource"),
+		EDIT_RESOURCE(1,Level.LEVEL_3,"Editer une ressource introduite par un autre utilisateur"),
+		REMOVE_RESOURCE(0,Level.LEVEL_3,"Supprimer une ressource introduite par un autre utilisateur"),
+		COMPLETE_RESOURCE(1, Level.LEVEL_1,"Completer les champs manquants d'une ressouce"),
+		EDIT_RESOURCE_URL(1, Level.LEVEL_4,"Editer les champs d'une ressouce"),
+		LINK_RESOURCE_TO_COMPETENCE(1, Level.LEVEL_4,"lier une ressouce à une compétence"),
+		ADD_PLAYLIST(2,Level.LEVEL_3,"Ajouter une playliste"),
+		ADD_PROBLEM(1, Level.LEVEL_1, "Signaler un problème"),
+		DISCUSS_PROBLEM(0, Level.LEVEL_1, "Discuter sur un problème"),
+		CLOSE_PROBLEM(3, Level.LEVEL_5, "Clôturer un problème");
+		
 		
 		int actionPoints;
 	    Level level;
