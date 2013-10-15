c974f79a7ea85634366c979aedf0388eb46552bd
john.rizzo.be@gmail.com <john.rizzo.be@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-10-15 13:02:36 +0000

    Admin sec
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@1036 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/AdminController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/AdminController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import learningresourcefinder.model.User;
+import learningresourcefinder.model.User.Role;
 import learningresourcefinder.repository.BaseRepository;
+import learningresourcefinder.security.SecurityContext;
 import learningresourcefinder.service.IndexManagerService;
 import learningresourcefinder.util.NotificationUtil;
 import learningresourcefinder.util.NotificationUtil.Status;
@@ -12,15 +14,17 @@ import learningresourcefinder.util.NotificationUtil.Status;
 @Controller
 public class AdminController extends BaseRepository<User> {
 	
-	@ Autowired IndexManagerService indexManagerService;
+	@Autowired IndexManagerService indexManagerService;
    
 	@RequestMapping("/admin")
 	public String admin() {
+	    SecurityContext.assertUserHasRole(Role.ADMIN);
 		return "admin";
     }
     
     @RequestMapping("/createindex")
     public String createIndex(){
+        SecurityContext.assertUserHasRole(Role.ADMIN);
     	indexManagerService.removeIndexes();
     	indexManagerService.createIndexes();
 		NotificationUtil.addNotificationMessage("Inexes supprimés puis recréés avec succcès", Status.SUCCESS);
