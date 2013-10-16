d7246209c1c5efa1600c35c1640363d95a0253ab
emilekane@gmail.com <emilekane@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2013-10-16 11:35:05 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@1047 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/TestController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/TestController.java
@@ -1,14 +1,20 @@
 package learningresourcefinder.controller;
 
+import java.util.List;
+
 import learningresourcefinder.model.BaseEntity;
 import learningresourcefinder.model.Competence;
+import learningresourcefinder.model.Cycle;
 import learningresourcefinder.repository.CompetenceRepository;
 import learningresourcefinder.repository.CycleRepository;
+import learningresourcefinder.service.CompetenceNodeService;
+import learningresourcefinder.util.CompetenceNode;
 import learningresourcefinder.util.NotificationUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -17,6 +23,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class TestController extends BaseController<BaseEntity>{
     @Autowired CycleRepository cycleRepository;
     @Autowired CompetenceRepository competenceRepository;
+    @Autowired CompetenceNodeService competenceNodeService;
     
     @RequestMapping("/test")
     public ModelAndView test() {
@@ -30,5 +37,5 @@ public class TestController extends BaseController<BaseEntity>{
         NotificationUtil.addNotificationMessage("Ceci est un message.");
         return new ModelAndView("test");
     }
-    
+ 
 }
