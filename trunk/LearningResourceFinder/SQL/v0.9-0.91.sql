acc18e7c493b06931b9b99a3e18f1487ce3a6038
john.rizzo.be@gmail.com <john.rizzo.be@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2014-01-02 09:39:48 +0000

    bat
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@1175 59821364-4b7c-4859-2266-30ef78c0912a

---
+++ trunk/LearningResourceFinder/SQL/v0.9-0.91.sql
@@ -0,0 +1,4 @@
+-- JOHN 2014-01-01
+
+alter table competence 
+        add column vraisforumpage varchar(255);
--- trunk/LearningResourceFinder/developer-client/restoreBackupDBFromServer.bat
+++ trunk/LearningResourceFinder/developer-client/restoreBackupDBFromServer.bat
@@ -2,14 +2,14 @@
 
 SET EXE_LOCATION=C:\Progra~1\PostgreSQL\9.2\bin
 SET USER=rycdev
-SET DATABASE=RYC
+SET DATABASE=LRF
 SET BACKUP_FILE=last-db
 SET PORT=2012
 
 %EXE_LOCATION%\dropdb.exe -U %USER% -p %PORT% %DATABASE%
 
 :: using the postgres user to create the DB, owned by the rycdev user. We could have used the rycdev user to create the dB, it was just in case of the rycdev user has no right to create DBs.
-%EXE_LOCATION%\createdb.exe -O %USER% -U postgres -p %PORT% %DATABASE%
+%EXE_LOCATION%\createdb.exe -O %USER% -U %USER% -p %PORT% %DATABASE%
 
 %EXE_LOCATION%\psql.exe -U %USER% -p %PORT% -f %BACKUP_FILE% %DATABASE% 
 
