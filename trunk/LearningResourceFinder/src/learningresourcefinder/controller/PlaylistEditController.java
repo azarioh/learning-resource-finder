c8ca93a4ab13a4475907f9ff8e842b02320e0141
Vincent06.Tallier@gmail.com <Vincent06.Tallier@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2014-08-08 08:08:28 +0000

    TP-23 Display name of deleted playlist instead of description into the notification message
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@1233 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/controller/PlaylistEditController.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/controller/PlaylistEditController.java
@@ -96,7 +96,7 @@ public class PlaylistEditController extends BaseController<PlayList>{
 	    SecurityContext.assertCurrentUserMayEditThisPlaylist(playlist);
 
 	    if (playlist!= null) {
-	        NotificationUtil.addNotificationMessage("Séquence "+ playlist.getDescription() + " bien supprimée", Status.SUCCESS);
+	        NotificationUtil.addNotificationMessage("Séquence "+ playlist.getName() + " bien supprimée", Status.SUCCESS);
 	        playlistRepository.remove(playlist);
 	    }
 
