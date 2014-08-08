db938e2447d67318fd2818760475e229a91e3a1b
alaingeorgespourvousservir@gmail.com <alaingeorgespourvousservir@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2014-08-08 07:46:59 +0000

    New right for level_1 users : validate a resource as "suitable for children". TP 27.
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@1230 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/util/Action.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/util/Action.java
@@ -6,6 +6,7 @@ public enum Action {
         ADD_RESOURCE(2, Level.LEVEL_1, "Ajouter une ressource"),  // Maybe later should be level 2.
         COMPLETE_RESOURCE(1, Level.LEVEL_1,"Completer les champs manquants d'une ressouce introduite par un autre utilisateur"),
         ADD_PROBLEM(1, Level.LEVEL_1, "Signaler un problème"),
+        VALIDATE_RESOURCE(1, Level.LEVEL_1, "Valider une ressource comme visible par des enfants"),
         DISCUSS_PROBLEM(0, Level.LEVEL_1, "Discuter sur un problème"),
         VOTE(1,Level.LEVEL_2, "Voter sur les ressources"),
 		EDIT_RESOURCE(1,Level.LEVEL_3,"Editer une ressource introduite par un autre utilisateur"),
