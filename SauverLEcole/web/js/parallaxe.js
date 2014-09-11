ee0078ee5277a06de3c11c4d3bf38371382d96e8
ahmed.zarioh@gmail.com <ahmed.zarioh@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2014-09-11 14:16:49 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@1391 59821364-4b7c-4859-2266-30ef78c0912a

---
+++ SauverLEcole/web/js/parallaxe.js
@@ -0,0 +1,84 @@
+		
+		var SCROLL_1 =2;
+		var SCROLL_2 =3;
+		var SCROLL_3 =5;
+		var PENTE = 0.36;
+		var scroll1;
+		var scroll2;
+		var scroll3;		
+		var bodyWidth;
+		var bodyHeight;
+		
+		var docElem = document.documentElement;
+
+
+		$(document).ready(function(){
+			getDimensions();
+			window.addEventListener('resize', function(event){
+				getDimensions();
+			});
+		});
+		function getDimensions(){
+			bodyHeight = docElem.clientHeight;
+			bodyWidth = docElem.clientWidth;
+		};
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		$(window).load(function() 
+		{
+			
+			$('#parallax-bg1').css('z-index',100);
+			$('#parallax-people').css('z-index',110);				
+			$('#parallax-bg2').css('z-index',120);		
+			$('#parallax-bg3').css('z-index',130);		
+		
+			calculDimensions() 
+			$("object.parralaxObject").width(bodyWidth);			
+			parallaxScroll();	
+			window.addEventListener('resize', function(event)
+			{
+				calculDimensions();				
+				$("object.parralaxObject").width(bodyWidth);
+				parallaxScroll();
+			});
+		});
+		
+		function calculDimensions() 
+		{
+			/*bodyWidth = $(window).width();
+			bodyHeight = $("body").height();
+			*/
+			
+			bodyHeight = docElem.clientHeight;
+			bodyWidth = docElem.clientWidth;
+			
+			scroll1 = SCROLL_1*bodyWidth/100;
+			scroll2 = SCROLL_2*bodyWidth/100;
+			scroll3 = SCROLL_3*bodyWidth/100;				
+		}
+
+		$(window).bind('scroll',function(e)
+		{
+			parallaxScroll();
+		});
+
+		function parallaxScroll()
+		{
+			var scrolled = $(window).scrollTop();
+			bodyHeight = $("body").height();
+			$('#parallax-bg1').css('bottom',(scrolled/bodyHeight)*scroll1-scroll1+'px');
+			$('#parallax-bg2').css('bottom',(scrolled/bodyHeight)*scroll2-scroll2+'px');
+			$('#parallax-bg3').css('bottom',(scrolled/bodyHeight)*scroll3-scroll3-2+'px');
+			
+			
+			$('#parallax-people').css('bottom',(scrolled/bodyHeight)*(scroll1+(bodyWidth/20))-scroll1+'px');
+			$('#parallax-people').css('left',(scrolled/bodyHeight)*(scroll1+(bodyWidth/10))+'px');
+		}
