5238ed6e0f3d2db762449d91b10b8557d5102f18
alaingeorgespourvousservir@gmail.com <alaingeorgespourvousservir@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2014-09-16 09:49:52 +0000

    crawler http://www.toutsavoir-hatier.com/
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@1456 59821364-4b7c-4859-2266-30ef78c0912a

---
+++ trunk/LearningResourceFinder/src/learningresourcefinder/service/crawler/CrawlerToutSavoir.java
@@ -0,0 +1,169 @@
+package learningresourcefinder.service.crawler;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import learningresourcefinder.model.Resource;
+import learningresourcefinder.model.UrlResource;
+import learningresourcefinder.model.Resource.Topic;
+import learningresourcefinder.repository.ResourceRepository;
+import learningresourcefinder.repository.UrlResourceRepository;
+import learningresourcefinder.search.SearchOptions.Format;
+import learningresourcefinder.search.SearchOptions.Language;
+import learningresourcefinder.search.SearchOptions.Platform;
+import learningresourcefinder.security.SecurityContext;
+
+import org.apache.poi.hssf.record.chart.CatLabRecord;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Transactional
+@Service
+public class CrawlerToutSavoir 
+{
+    @Autowired  ResourceRepository resourceRepository ;
+    @Autowired  UrlResourceRepository urlResourceRepository ;
+    @Autowired CrawlerService crawlerService;
+    //DONE
+    public void crawler() throws IOException
+    {
+        Document frontPage = Jsoup.connect("http://www.toutsavoir-hatier.com").timeout(10000).get();
+        Elements elements = frontPage.select(".lienblancns").select("a");    //Selectionne CP CM... liens de milieu de page
+        for (Element element : elements)  
+        {
+            System.out.println("=======================================================================");
+            System.out.println(element.text());
+            System.out.println("=======================================================================");
+            String mainLink = "http://www.toutsavoir-hatier.com/" + element.attr("href");
+
+            Document categoryDoc = Jsoup.connect(mainLink).timeout(10000).get();
+            Elements elementsByCategory = categoryDoc.select(".lienblancns").select("a");  // Selectionne Francais Maths... + CP CM1 en bas de page
+
+            for (Element elementByCategory : elementsByCategory)  
+            {
+
+                String category = "------------------";
+                String title = "-----------------------";
+                String link = null;
+                String goToCategory = "http://www.toutsavoir-hatier.com/" + elementByCategory.attr("href");
+
+                if (!goToCategory.contains("ecole")) // Pour éviter les liens CP CM1... de bas de page - garde les liens année/matière
+                {
+
+                    Document sommaireExosByCategory = Jsoup.connect(goToCategory).timeout(10000).get();  
+                    Elements exosList= sommaireExosByCategory.select("a");
+
+                    for (Element linkFinal : exosList) 
+                    {
+                        if (linkFinal.attr("href").contains("exo_entrainement")) {  // exo_entrainement: nomenclature commune math francais
+
+                            String goToFinalLinkMathFrench = "http://www.toutsavoir-hatier.com/" + linkFinal.attr("href");
+                            Document mathFrenchDoc = Jsoup.connect(goToFinalLinkMathFrench).timeout(10000).get();
+                            Elements mathFrenchExos = mathFrenchDoc.select(".enonce");
+                            for ( Element mathFrenchExo : mathFrenchExos) {
+
+
+                                title = mathFrenchExo.select(".enonce").text();
+                                link = goToFinalLinkMathFrench; 
+                                if (goToCategory.contains("id_mat=2")) {
+                                    category = "math";
+                                }
+                                else if (goToCategory.contains("id_mat=1")) {
+                                    category = "français";
+                                }
+                            }
+
+                        }
+
+                        else if (linkFinal.attr("href").contains("fiche_dictee")) {  
+
+                            String goToFinalLinkDictees = "http://www.toutsavoir-hatier.com/" + linkFinal.attr("href");
+                            Document dicteesDoc = Jsoup.connect(goToFinalLinkDictees).timeout(10000).get();
+                            Elements dicteesExos = dicteesDoc.select(".notion");
+                            for ( Element dicteesExo : dicteesExos) {
+
+
+                                title = dicteesExo.select(".notion").text();
+                                link = goToFinalLinkDictees;
+                                category = "français";
+                            }
+
+                        }
+                        
+
+                        else if (linkFinal.attr("href").contains("liste_podcasts")) {  
+
+                            String goToFinalLinkEnglishEasy = "http://www.toutsavoir-hatier.com/" + linkFinal.attr("href");
+                          
+
+
+                                title = linkFinal.text();
+                                link = goToFinalLinkEnglishEasy;
+                                category = "anglais";
+                           
+
+                        }
+                        else if (linkFinal.attr("href").contains("workshop")) {  
+                            String goToFinalLinkEnglish = "http://www.toutsavoir-hatier.com/" + linkFinal.attr("href");
+
+                                title = linkFinal.text();
+                                link = goToFinalLinkEnglish;
+                                category = "anglais";
+                        }
+                        if(link!=null)
+                        {
+                            System.out.println(title);
+                            System.out.println(link);
+                            System.out.println(category);
+
+
+                            
+                              
+                        Resource r = new Resource(crawlerService.getSubString(title,50),"Exercice Tout Savoir Hatier",SecurityContext.getUser());
+                        r.setLanguage(Language.FR);
+
+                        Topic topic = CrawlerService.getTopicFromString(category);
+                        topic = (topic!=null)?topic:crawlerService.getTopicFromString(title);                  
+                        r.setTopic( topic);           
+                        r.setFormat(Format.INTERACTIVE);
+                        Set<Platform> tempSet = new HashSet<>();
+                        tempSet.add(Platform.BROWSER);
+                        r.setPlatforms(tempSet);
+                        UrlResource url = new UrlResource(crawlerService.getSubString(title,50),link,r);
+
+                            resourceRepository.persist(r);
+                            urlResourceRepository.persist(url);
+                            r.getShortId(); 
+
+
+
+                             
+
+                        }
+                        category = "------------------";
+                        title = "-----------------------";
+                        link = null;
+
+                    }
+
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException 
+    {
+        CrawlerToutSavoir ts = new CrawlerToutSavoir();
+        ts.crawler();
+
+    }
+
+}
