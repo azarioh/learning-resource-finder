401a2740cae342e73a8803bf1fcac313677ce84d
guillaumep.isat@gmail.com <guillaumep.isat@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2014-09-26 07:17:06 +0000

    "/" or not
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@1484 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/src/learningresourcefinder/repository/UrlGenericRepository.java
+++ trunk/LearningResourceFinder/src/learningresourcefinder/repository/UrlGenericRepository.java
@@ -16,6 +16,11 @@ public class UrlGenericRepository extends BaseRepository<UrlGeneric> {
     }
 
     public UrlGeneric findByurl(String url) {
+    	//because copy-paste add '/' char at the end of the URL 
+    	//allow only one URL format in DB (without '/' char at end of it)
+    	if(url.endsWith("/")){
+    		url = url.substring(0, url.length()-1);
+    	}
         return getSingleOrNullResult(em.createQuery("select u from UrlGeneric u where u.url =:url").setParameter("url", url));
     }
 
