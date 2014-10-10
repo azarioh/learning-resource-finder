09a77e72ff14c045da51a2dbaaea8602289381cb
brunobp7@gmail.com <brunobp7@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2014-10-10 09:22:17 +0000

    git-svn-id: https://learning-resource-finder.googlecode.com/svn@1605 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/WebContent/js/int/resourceIncreaseDiv.js
+++ trunk/LearningResourceFinder/WebContent/js/int/resourceIncreaseDiv.js
@@ -3,10 +3,10 @@ var toFrontZIndex = 2;
 $(document).ready(function() {
 	$(".resource-content > .panel-heading").click(function() {
       	if ($(this).parent().hasClass("resource-content-big")) { // If is small
-      		$(this).parent().switchClass("resource-content-big", "resource-content", 10);  // Big will be small
+      		$(this).parent().switchClass("resource-content-big", "resource-content", 0);  // Big will be small
       		$(this).parent().css("z-index", "1");
       	} else {
-            $(this).parent().switchClass("resource-content", "resource-content-big", 100); // Small will be big
+            $(this).parent().switchClass("resource-content", "resource-content-big", 0); // Small will be big
             $(this).parent().css("z-index", toFrontZIndex++);  // to front (we make it more and more to front in case user opens multiple boxes that overlap
       	}
 	});
