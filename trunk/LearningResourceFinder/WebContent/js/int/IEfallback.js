7f528d73ef1d3163a5cd8e347f32247c939ebe63
njboyd@gmail.com <njboyd@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2014-11-06 10:35:44 +0000

    Now testing via conditional javascript.
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@1813 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/WebContent/js/int/IEfallback.js
+++ trunk/LearningResourceFinder/WebContent/js/int/IEfallback.js
@@ -1,6 +1,6 @@
 /*@cc_on
  
-@if (@_jscript_version > 10)
+@if (@_jscript_version <= 10)
 	var logo = document.getElementById("mainLogo");
 	logo.setAttribute("class", "mainLogoFallback");
 	logo.setAttribute("src", "/images/ToujoursPlus-logo-");
