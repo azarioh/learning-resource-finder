007e97b2f35c13d98b28ec697aed3e85a464f10e
njboyd@gmail.com <njboyd@gmail.com@59821364-4b7c-4859-2266-30ef78c0912a>
2014-11-06 10:32:10 +0000

    Now testing via conditional javascript.
    git-svn-id: https://learning-resource-finder.googlecode.com/svn@1812 59821364-4b7c-4859-2266-30ef78c0912a

--- trunk/LearningResourceFinder/WebContent/js/int/IEfallback.js
+++ trunk/LearningResourceFinder/WebContent/js/int/IEfallback.js
@@ -1,7 +1,9 @@
-$(document).ready(function(){
-	if (document.all) {
-	    var logo = document.getElementById("mainLogo");
-	    logo.setAttribute("class", "mainLogoFallback");
-	    logo.setAttribute("src", "/images/ToujoursPlus-logo-500px.png");
-	}
-});
+/*@cc_on
+ 
+@if (@_jscript_version > 10)
+	var logo = document.getElementById("mainLogo");
+	logo.setAttribute("class", "mainLogoFallback");
+	logo.setAttribute("src", "/images/ToujoursPlus-logo-");
+@end
+	    
+@*/
